a1655b37378a3338c1b56b50b7aaeea909f8edec
Ahmed ElSayed <ahmels@microsoft.com>
2017-01-11 12:20:59 -0800

    Updating .gitignore

---
+++ test/Azure.Functions.Cli.Tests/ActionsTests/CreateFunctionActionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Azure.Functions.Cli.Tests.ActionsTests
+{
+    public class CreateFunctionActionTests : ActionTestsBase
+    {
+        public CreateFunctionActionTests(ITestOutputHelper output) : base(output) { }
+
+        [Theory]
+        [InlineData("c#", "httpTrigger", "newFunc")]
+        public async Task CreateFunctionActionTest(string language, string templateName, string FunctionName)
+        {
+        }
+    }
+}
