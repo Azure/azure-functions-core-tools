64a6ec0050b3ed09635b30ceafd4631601252d18
Ahmed ElSayed <ahmels@microsoft.com>
2017-02-03 14:49:35 -0800

    Update version to 1.0.0.beta.10

--- src/Azure.Functions.Cli/Common/Constants.cs
+++ src/Azure.Functions.Cli/Common/Constants.cs
@@ -6,7 +6,7 @@
         public const string FunctionsStorageAccountNamePrefix = "AzureFunctions";
         public const string StorageAccountArmType = "Microsoft.Storage/storageAccounts";
         public const string FunctionAppArmKind = "functionapp";
-        public const string CliVersion = "1.0.0-beta.9";
+        public const string CliVersion = "1.0.0-beta.10";
         public const string CliDebug = "CLI_DEBUG";
 
         public static class Errors
