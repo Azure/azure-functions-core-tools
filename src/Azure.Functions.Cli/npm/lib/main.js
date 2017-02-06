8062cba4b18c69b20f7e3594f66493e0beb9dd8b
Ahmed ElSayed <ahmels@microsoft.com>
2017-02-06 12:56:00 -0800

    Update npm bootstrap script

--- src/Azure.Functions.Cli/npm/lib/main.js
+++ src/Azure.Functions.Cli/npm/lib/main.js
@@ -8,18 +8,19 @@ var args = process.argv;
 
 function main() {
     var isWin = /^win/.test(process.platform);
-    if (!isWin) {
-        console.log('Currently all the features are only supported in Windows.\n');
-        console.log('"azurefunctions new" is the only feature working across platforms.\n');
-        console.log('Follow https://github.com/Azure/azure-webjobs-sdk-script/issues/509 for updates.');
-        process.exit(1);
-    }
     var bin = path.join(path.dirname(fs.realpathSync(__filename)), '../bin');
-    var funcProc = spawn(bin + '/func.exe', args.slice(2), { stdio : [process.stdin, process.stdout, process.stderr, 'pipe']});
 
-    funcProc.on('exit', function(code) {
-        process.exit(code);
-    });
+    if (!isWin) {
+        var funcProc = spawn("mono" + bin + '/func.exe', args.slice(2), { stdio : [process.stdin, process.stdout, process.stderr, 'pipe']});
+        funcProc.on('exit', function(code) {
+            process.exit(code);
+        });
+    } else {
+        var funcProc = spawn(bin + '/func.exe', args.slice(2), { stdio : [process.stdin, process.stdout, process.stderr, 'pipe']});
+        funcProc.on('exit', function(code) {
+            process.exit(code);
+        });
+    }
 }
 
 main();
