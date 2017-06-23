5876bd67b05044a9f683d447b9b1cb66df35fb32
jaredcnance <jaredcnance@gmail.com>
2017-06-23 08:59:51 -0500

    login action tests

---
+++ test/Azure.Functions.Cli.Tests/ActionsTests/LoginActionTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Functions.Cli.Actions.AzureActions;
+using Azure.Functions.Cli.Arm;
+using Azure.Functions.Cli.Interfaces;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Azure.Functions.Cli.Tests.ActionsTests
+{
+    public class LoginActionTests : ActionTestsBase
+    {
+        public LoginActionTests(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public async Task Defaults_To_Interactive_Login()
+        {
+            var armManagerMock = new Mock<IArmManager>();
+            var settingsMock = new Mock<ISettings>();
+            var action = new LoginAction(armManagerMock.Object, settingsMock.Object);
+            var args = new string[0];
+
+            // Test
+            action.ParseArgs(args);
+            await action.RunAsync();
+
+            // Assert
+            armManagerMock.Verify(m => m.LoginAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task Can_Login_With_Username_And_Password()
+        {
+            var armManagerMock = new Mock<IArmManager>();
+            var settingsMock = new Mock<ISettings>();
+            var action = new LoginAction(armManagerMock.Object, settingsMock.Object);
+            var username = Guid.NewGuid().ToString();
+            var password = Guid.NewGuid().ToString();
+            var args = new [] { "-u", username , "-w", password };
+
+            // Test
+            action.ParseArgs(args);
+            await action.RunAsync();
+
+            // Assert
+            armManagerMock.Verify(m => m.LoginAsync(
+                It.Is<string>(s => s == username),
+                It.Is<string>(s => s == password)
+            ), Times.Once);
+        }
+    }
+}
