9fb718146435abcc7812eda96ac047caa3345b6f
Ahmed ElSayed <ahmels@microsoft.com>
2017-06-29 17:48:09 -0700

    Update version to 1.0.0-beta.100

--- src/Azure.Functions.Cli/Common/Constants.cs
+++ src/Azure.Functions.Cli/Common/Constants.cs
@@ -6,7 +6,7 @@
         public const string FunctionsStorageAccountNamePrefix = "AzureFunctions";
         public const string StorageAccountArmType = "Microsoft.Storage/storageAccounts";
         public const string FunctionAppArmKind = "functionapp";
-        public const string CliVersion = "1.0.0-beta.99";
+        public const string CliVersion = "1.0.0-beta.100";
         public const string CliDebug = "CLI_DEBUG";
         public const string DefaultSqlProviderName = "System.Data.SqlClient";
 
