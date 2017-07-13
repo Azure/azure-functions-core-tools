ce7f1c036d05ea03ee4667178a61a70f9ba8263f
Ahmed ElSayed <ahmels@microsoft.com>
2017-07-13 11:50:14 -0700

    throw a better error message for non-windows machines. Fixes #168

--- src/Azure.Functions.Cli/npm/lib/main.js
+++ src/Azure.Functions.Cli/npm/lib/main.js
@@ -11,10 +11,9 @@ function main() {
     var bin = path.join(path.dirname(fs.realpathSync(__filename)), '../bin');
 
     if (!isWin) {
-        var funcProc = spawn("mono " + bin + '/func.exe', args.slice(2), { stdio : [process.stdin, process.stdout, process.stderr, 'pipe']});
-        funcProc.on('exit', function(code) {
-            process.exit(code);
-        });
+        console.log('Currently Azure Functions Core Tools is Windows only.\n');
+        console.log('Follow https://github.com/Azure/azure-functions-cli/issues/178 for updates.');
+        process.exit(1);
     } else {
         var funcProc = spawn(bin + '/func.exe', args.slice(2), { stdio : [process.stdin, process.stdout, process.stderr, 'pipe']});
         funcProc.on('exit', function(code) {
