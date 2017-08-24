bc115f3cbaae751035249b5367555560d067c627
Ahmed ElSayed <ahmels@microsoft.com>
2017-08-24 13:52:23 -0700

    Write cli to user profile instead of global path to not require sudo on linux

--- src/Azure.Functions.Cli/npm/lib/main.js
+++ src/Azure.Functions.Cli/npm/lib/main.js
@@ -5,13 +5,16 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 var fork = require('child_process').fork;
 var commandExists = require('command-exists');
+var os = require('os');
 var args = process.argv;
 
 function main() {
     commandExists('dotnet', function (err, commandExists) {
         if (commandExists) {
-            var bin = path.join(path.dirname(fs.realpathSync(__filename)), '../bin');
-            var funcProc = spawn('dotnet', [bin + '/Azure.Functions.Cli.dll',].concat(args.slice(2)), { stdio: [process.stdin, process.stdout, process.stderr, 'pipe'] });
+            var bin = path.join(os.homedir(), '.azurefunctions', 'bin');
+            var funcProc = spawn('dotnet', [bin + '/Azure.Functions.Cli.dll', ].concat(args.slice(2)), {
+                stdio: [process.stdin, process.stdout, process.stderr, 'pipe']
+            });
             funcProc.on('exit', function (code) {
                 process.exit(code);
             });
