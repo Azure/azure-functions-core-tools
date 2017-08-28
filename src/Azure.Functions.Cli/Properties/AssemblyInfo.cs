e3e33d1e2ee0313db2bed7acb8a6feb4a0cf3e34
Ahmed ElSayed <ahmels@microsoft.com>
2017-08-28 15:15:44 -0700

    Fix build script for core branch

--- src/Azure.Functions.Cli/Properties/AssemblyInfo.cs
+++ src/Azure.Functions.Cli/Properties/AssemblyInfo.cs
@@ -1,15 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("17cb554c-1af5-47b9-960c-32e8fe516544")] 
-
-[assembly: InternalsVisibleTo("Azure.Functions.Cli.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleToAttribute("Azure.Functions.Cli.Tests")]
+[assembly: InternalsVisibleToAttribute("DynamicProxyGenAssembly2")]
