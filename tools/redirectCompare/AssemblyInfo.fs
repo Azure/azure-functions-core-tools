6014bd1bab7faf15e51bf8371896389eb099bd31
Ahmed ElSayed <ahmels@microsoft.com>
2017-09-15 11:37:18 -0700

    Update runtime to 11232, update CLI to 1.0.4, add redirectCompare

---
+++ tools/redirectCompare/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace redirectCompare.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("redirectCompare")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("redirectCompare")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("dfe7f321-00d7-47e0-8653-6329cde3d78c")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ tools/redirectCompare/Program.fs
@@ -0,0 +1,35 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System.IO
+open FSharp.Data
+open System.Net
+
+type Config = XmlProvider<"""
+<configuration>
+  <runtime>
+    <assemblyBinding xmlns="urn:schemas-microsoft-com:asm.v1">
+      <dependentAssembly>
+        <assemblyIdentity name="" publicKeyToken="" culture="" />
+        <bindingRedirect oldVersion="" newVersion="" />
+      </dependentAssembly>
+      <dependentAssembly>
+        <assemblyIdentity name="" publicKeyToken="" culture="" />
+        <bindingRedirect oldVersion="" newVersion="" />
+      </dependentAssembly>
+    </assemblyBinding>
+  </runtime>
+</configuration>
+""">
+
+let source = new WebClient () |> fun x -> x.DownloadString("https://raw.githubusercontent.com/Azure/azure-webjobs-sdk-script/dev/src/WebJobs.Script.WebHost/Web.config") |> Config.Parse
+let dest = "..\\..\\..\\..\\src\\Azure.Functions.Cli\\App.config" |> File.ReadAllText |> Config.Parse
+
+for assembly in source.Runtime.AssemblyBinding.DependentAssemblies do
+    let destElement =
+        dest.Runtime.AssemblyBinding.DependentAssemblies
+        |> Array.tryFind (fun x -> x.AssemblyIdentity.Name = assembly.AssemblyIdentity.Name)
+    if destElement.IsSome && (destElement.Value.BindingRedirect.NewVersion <> assembly.BindingRedirect.NewVersion || destElement.Value.BindingRedirect.OldVersion <> assembly.BindingRedirect.OldVersion) then
+        printfn "Different: %s" destElement.Value.AssemblyIdentity.Name
+    else if destElement.IsNone then
+        printfn "Not in dest: %s" assembly.AssemblyIdentity.Name
