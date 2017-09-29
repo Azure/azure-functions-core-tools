78bce59eaf130bc32a534ebc0766a286e162f5b9
Matt Mason <mamaso@microsoft.com>
2017-09-29 14:20:33 -0700

    extensions sync and rename add -> install

---
+++ src/Azure.Functions.Cli/Actions/LocalActions/InstallExtensionAction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Functions.Cli.Interfaces;
+using Colors.Net;
+using Fclp;
+using System.Runtime.InteropServices;
+using static Azure.Functions.Cli.Common.OutputTheme;
+using Azure.Functions.Cli.Common;
+using System.IO;
+using Azure.Functions.Cli.Helpers;
+
+namespace Azure.Functions.Cli.Actions.LocalActions
+{
+    [Action(Name = "install", Context = Context.Extensions, HelpText = "Installs a function extension in a function app.")]
+    internal class InstallExtensionAction : BaseAction
+    {
+        public string Package { get; set; }
+        public string Version { get; set; }
+
+        public string OutputPath { get; set; }
+
+        public InstallExtensionAction()
+        {
+        }
+
+        public override ICommandLineParserResult ParseArgs(string[] args)
+        {
+            Parser
+                .Setup<string>('p', "package")
+                .WithDescription("Extension package")
+                .Callback(p => Package = p)
+                .Required();
+
+            Parser
+                .Setup<string>('v', "version")
+                .WithDescription("Extension version")
+                .Callback(v => Version = v)
+                .Required();
+
+            Parser
+                .Setup<string>('o', "output")
+                .WithDescription("Output path")
+                .SetDefault(Path.Combine(".", "bin"))
+                .Callback(o => OutputPath = Path.GetFullPath(o));
+
+            return Parser.Parse(args);
+        }
+
+        public async override Task RunAsync()
+        {
+            var extensionsProj = await ExtensionsHelper.EnsureExtensionsProjectExistsAsync();
+
+            var addPackage = new Executable("dotnet", $"add {extensionsProj} package {Package} --version {Version}");
+            await addPackage.RunAsync(output => ColoredConsole.WriteLine(output), error => ColoredConsole.WriteLine(ErrorColor(error)));
+
+            var syncAction = new SyncExtensionsAction()
+            {
+                OutputPath = OutputPath
+            };
+
+            await syncAction.RunAsync();
+        }
+    }
+}
---
+++ src/Azure.Functions.Cli/Actions/LocalActions/SyncExtensionsAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Functions.Cli.Common;
+using Azure.Functions.Cli.Helpers;
+using static Azure.Functions.Cli.Common.OutputTheme;
+using Colors.Net;
+using Fclp;
+
+namespace Azure.Functions.Cli.Actions.LocalActions
+{
+    [Action(Name = "sync", Context = Context.Extensions, HelpText = "Installs all extensions added to the function app.")]
+    internal class SyncExtensionsAction : BaseAction
+    {
+        public string Package { get; set; }
+        public string Version { get; set; }
+
+        public string OutputPath { get; set; }
+
+        public override ICommandLineParserResult ParseArgs(string[] args)
+        {
+            Parser
+                .Setup<string>('o', "output")
+                .WithDescription("Output path")
+                .SetDefault(Path.Combine(".", "bin"))
+                .Callback(o => OutputPath = Path.GetFullPath(o));
+
+            return Parser.Parse(args);
+        }
+
+        public async override Task RunAsync()
+        {
+            var extensionsProj = await ExtensionsHelper.EnsureExtensionsProjectExistsAsync();
+
+            var installExtensions = new Executable("dotnet", $"build {extensionsProj} -o {OutputPath}");
+            await installExtensions.RunAsync(output => ColoredConsole.WriteLine(output), error => ColoredConsole.WriteLine(ErrorColor(error)));
+        }
+    }
+}
