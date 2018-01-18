7646c3c08a70540e1294a6daa08e47b73dafd873
Ahmed ElSayed <ahmels@microsoft.com>
2018-01-17 16:42:18 -0800

    Add packageConfigParser

---
+++ packageConfigParser/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+
+namespace packageConfigParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParsePackageConfig("https://raw.githubusercontent.com/Azure/azure-webjobs-sdk-script/v1.x/src/WebJobs.Script.WebHost/packages.config");
+            ParsePackageConfig("https://raw.githubusercontent.com/Azure/azure-webjobs-sdk-script/v1.x/src/WebJobs.Script/packages.config");
+        }
+
+        static void ParsePackageConfig(string url)
+        {
+            using (var client = new WebClient())
+            {
+                var content = client.DownloadString(new Uri(url));
+                var doc = XDocument.Parse(content);
+                var packages = doc
+                    .Element("packages")
+                    .Descendants()
+                    .Select(e => new
+                    {
+                        Name = e.Attribute("id").Value,
+                        Version = e.Attribute("version").Value
+                    })
+                    .Where(i => i.Name != "StyleCop.Analyzers");
+
+                foreach (var p in packages)
+                {
+                    Console.WriteLine($"Install-Package {p.Name} -Version {p.Version}");
+                }
+            }
+        }
+    }
+}
