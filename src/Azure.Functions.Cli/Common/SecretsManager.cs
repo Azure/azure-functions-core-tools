332c088759de50225f53bdcab43ef92edbb2c930
George Wilson <georgejameswilson@googlemail.com>
2018-01-24 11:04:07 +0000

    Remove import

--- src/Azure.Functions.Cli/Common/SecretsManager.cs
+++ src/Azure.Functions.Cli/Common/SecretsManager.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
-using Azure.Functions.Cli.Common;
 using Azure.Functions.Cli.Interfaces;
 using Azure.Functions.Cli.Helpers;
 using Newtonsoft.Json.Linq;
