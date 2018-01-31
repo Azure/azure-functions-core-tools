6573e9868cfaf2e7ad74c18fb1571e7b6e9b741f
Ahmed ElSayed <ahmels@microsoft.com>
2018-01-31 15:59:28 -0800

    call clean on x86 and x64

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 .paket\paket.exe install
 
-packages\FAKE\tools\fake .\build.fsx clean
+packages\FAKE\tools\fake .\build.fsx clean platform=x86
+packages\FAKE\tools\fake .\build.fsx clean platform=x64
+
 packages\FAKE\tools\fake .\build.fsx platform=x86 -ev sign
 packages\FAKE\tools\fake .\build.fsx platform=x64 -ev sign
