3f562146140929ffcda7cf4e20bda3b89563f44e
Ahmed ElSayed <ahmels@microsoft.com>
2018-04-27 10:20:47 -0700

    add functions.js to project

--- build.fsx
+++ build.fsx
@@ -161,6 +161,7 @@ Target "GenerateZipToSign" (fun _ ->
 
     !! (buildDir @@ "/**/Microsoft.Azure.*.dll")
         ++ (buildDir @@ "func.exe")
+        ++ (buildDir @@ "azurefunctions/functions.js")
         ++ (buildDir @@ "azurefunctions/http/request.js")
         ++ (buildDir @@ "azurefunctions/http/response.js")
         |> notSigned
@@ -246,6 +247,7 @@ Target "WaitForSigning" (fun _ ->
     match signed with
     | Success file ->
         Unzip buildDir file
+        MoveFile (buildDir @@ "azurefunctions/") (buildDir @@ "functions.js")
         MoveFile (buildDir @@ "azurefunctions/http/") (buildDir @@ "request.js")
         MoveFile (buildDir @@ "azurefunctions/http/") (buildDir @@ "response.js")
     | Failure e -> targetError e null |> ignore
