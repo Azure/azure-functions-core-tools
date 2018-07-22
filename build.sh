867c54c32560e704066036e66e1eb0b493e9c067
Ahmed ElSayed <ahmels@microsoft.com>
2018-07-21 17:26:00 -0700

    Update linux build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dotnet build Azure.Functions.Cli.sln
-dotnet publish test/Azure.Functions.Cli.Tests/Azure.Functions.Cli.Tests.csproj --runtime linux-x64 --output /tmp/cli
+dotnet publish src/Azure.Functions.Cli/Azure.Functions.Cli.csproj --runtime linux-x64 --output /tmp/cli
 FUNC_PATH=/tmp/cli/func dotnet test test/Azure.Functions.Cli.Tests/Azure.Functions.Cli.Tests.csproj
