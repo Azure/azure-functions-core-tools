1a862961aa28c0ef4a23f4a761eb18f6a145e1fc
yaron2 <schneider.yaron@live.com>
2018-07-26 16:54:44 -0700

    added func logs functionality

---
+++ src/Azure.Functions.Cli/Actions/LogsActions/GetLogsAction.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Functions.Cli.Actions.DeployActions.Platforms;
+using Azure.Functions.Cli.Common;
+using Azure.Functions.Cli.Helpers;
+using Azure.Functions.Cli.Interfaces;
+using Colors.Net;
+using Fclp;
+using Microsoft.Azure.WebJobs.Script;
+using static Azure.Functions.Cli.Common.OutputTheme;
+using static Colors.Net.StringStaticMethods;
+
+namespace Azure.Functions.Cli.Actions.LocalActions
+{
+    [Action(Name = "logs", HelpText = "Gets logs of Functions running on custom backends")]
+    internal class GetLogsAction : BaseAction
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Platform { get; set; } = string.Empty;
+        public string ConfigPath { get; set; } = string.Empty;
+        private Dictionary<string, Func<string, Task>> logsHandlersMap = new Dictionary<string, Func<string, Task>>();
+        private const string KUBERNETES_DEFAULT_NAMESPACE = "azure-functions";
+
+        private readonly ITemplatesManager _templatesManager;
+
+        public GetLogsAction(ITemplatesManager templatesManager)
+        {
+            _templatesManager = templatesManager;
+            LoadLogHandlers();
+        }
+
+        private void LoadLogHandlers()
+        {
+            logsHandlersMap.Add("kubernetes", this.GetKubernetesFunctionLogs);
+        }
+
+        public override ICommandLineParserResult ParseArgs(string[] args)
+        {
+            Parser
+                .Setup<string>("platform")
+                .WithDescription("Hosting platform for the function app. Valid options: " + String.Join(",", logsHandlersMap.Keys))
+                .Callback(t => Platform = t).Required();
+
+            Parser
+                .Setup<string>("name")
+                .WithDescription("Function name")
+                .Callback(t => Name = t).Required();
+
+            Parser
+                .Setup<string>("config")
+                .WithDescription("[Optional] Config file")
+                .Callback(t => ConfigPath = t);
+
+            return base.ParseArgs(args);
+        }
+
+        public async Task GetKubernetesFunctionLogs(string functionName)
+        {
+            string nameSpace = KUBERNETES_DEFAULT_NAMESPACE;
+            await KubernetesHelper.RunKubectl($"logs -l app={functionName}-deployment -n {nameSpace}", true);
+        }
+
+        public override async Task RunAsync()
+        {
+            if (!logsHandlersMap.ContainsKey(Platform))
+            {
+                ColoredConsole.Error.WriteLine(ErrorColor($"platform {Platform} is not supported. Valid options are: {String.Join(",", logsHandlersMap.Keys)}"));
+                return;
+            }            
+
+            var logsHandler = logsHandlersMap[Platform];
+            ColoredConsole.WriteLine("Function logs:");
+            await logsHandler(Name);
+            
+        }
+    }
+}
