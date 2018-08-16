b82c2435b2302dca6949f03d30a12bcd006e7457
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-08-16 13:11:51 -0700

    Configuring AI telemetry client SDK with the CLI information

--- src/Azure.Functions.Cli/Diagnostics/LoggingBuilder.cs
+++ src/Azure.Functions.Cli/Diagnostics/LoggingBuilder.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
+using Azure.Functions.Cli.Common;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using Microsoft.Azure.WebJobs.Script;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Azure.Functions.Cli.Diagnostics
@@ -9,6 +15,16 @@ namespace Azure.Functions.Cli.Diagnostics
         public void Configure(ILoggingBuilder builder)
         {
             builder.AddProvider(new ColoredConsoleLoggerProvider((cat, level) => level >= LogLevel.Information));
+
+            builder.Services.AddSingleton<TelemetryClient>(provider =>
+            {
+                TelemetryConfiguration configuration = provider.GetService<TelemetryConfiguration>();
+                TelemetryClient client = new TelemetryClient(configuration);
+
+                client.Context.GetInternalContext().SdkVersion = $"azurefunctionscoretools: {Constants.CliVersion}";
+
+                return client;
+            });
         }
     }
 }
