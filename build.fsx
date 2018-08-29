7e4439ad2ac2cd4487e8d92f8df971803f74887b
Naren Soni <soninaren@live.com>
2018-08-28 20:18:04 -0700

    Updating the templates and WebHost reference

--- build.fsx
+++ build.fsx
@@ -329,8 +329,8 @@ Target "AddTemplatesNupkgs" (fun _ ->
     let itemTemplates = (release.["itemTemplates"]).AsString()
     let projectTemplates = (release.["projectTemplates"]).AsString()
 
-    let itemTemplates = "https://www.myget.org/F/azure-appservice/api/v2/package/Azure.Functions.Templates/2.0.0-beta-10224"
-    let projectTemplates = "https://www.myget.org/F/azure-appservice/api/v2/package/Microsoft.AzureFunctions.ProjectTemplates/2.0.0-beta-10224"
+    let itemTemplates = "https://www.myget.org/F/azure-appservice/api/v2/package/Azure.Functions.Templates/2.0.0-beta-10231"
+    let projectTemplates = "https://www.myget.org/F/azure-appservice/api/v2/package/Microsoft.AzureFunctions.ProjectTemplates/2.0.0-beta-10231"
 
     use webClient = new WebClient ()
     [
