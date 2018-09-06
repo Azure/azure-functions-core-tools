c72edc14e8d6a4c90836078eafab1de66fdefd7f
Ankit Kumar <ankikuma@microsoft.com>
2018-09-06 15:27:58 -0700

    Fixes #701 - minor directory naming bug

--- src/Azure.Functions.Cli/Common/SelfHostWebHostSettingsFactory.cs
+++ src/Azure.Functions.Cli/Common/SelfHostWebHostSettingsFactory.cs
@@ -12,7 +12,7 @@ namespace Azure.Functions.Cli.Common
             {
                 IsSelfHost = true,
                 ScriptPath = scriptPath,
-                LogPath = Path.Combine(Path.GetTempPath(), @"LogFiles\Application\Functions"),
+                LogPath = Path.Combine(Path.GetTempPath(), "LogFiles", "Application", "Functions"),
                 SecretsPath = Path.Combine(Path.GetTempPath(), "secrets", "functions", "secrets")
             };
         }
