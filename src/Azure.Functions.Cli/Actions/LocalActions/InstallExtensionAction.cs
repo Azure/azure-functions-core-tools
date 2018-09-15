5f3d4379293255ea12aae840ecd960aa25617018
Naren Soni <soninaren@users.noreply.github.com>
2018-09-14 17:27:41 -0700

    Updating templates and webhost package, replace service bus package on install command, w (#723)

--- src/Azure.Functions.Cli/Actions/LocalActions/InstallExtensionAction.cs
+++ src/Azure.Functions.Cli/Actions/LocalActions/InstallExtensionAction.cs
@@ -71,7 +71,13 @@ namespace Azure.Functions.Cli.Actions.LocalActions
                     throw new CliArgumentsException("Invalid config path, please verify directory exists");
                 }
 
-                var extensionsProj = await ExtensionsHelper.EnsureExtensionsProjectExistsAsync(_secretsManager, Csx, ConfigPath);
+                var extensionsProj = ExtensionsHelper.GetExtensionsProjectPath(_secretsManager, Csx, ConfigPath);
+                if (FileSystemHelpers.FileExists(extensionsProj))
+                {
+                    FileSystemHelpers.FileDelete(extensionsProj);
+                }
+
+                extensionsProj = await ExtensionsHelper.EnsureExtensionsProjectExistsAsync(_secretsManager, Csx, ConfigPath);
 
                 if (string.IsNullOrEmpty(Package) && string.IsNullOrEmpty(Version))
                 {
