8002739b4ad052a456d0d47e288c595a63206e49
Naren Soni <soninaren@live.com>
2018-09-19 12:53:55 -0700

    Add optional force parameter to func extensions install command

---
+++ src/Azure.Functions.Cli/Helpers/ProjectHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.Build.Construction;
+using Azure.Functions.Cli.Common;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace Azure.Functions.Cli.Helpers
+{
+    internal static class ProjectHelpers
+    {
+        public static bool PackageReferenceExists(this ProjectRootElement project, string packageId)
+        {
+            ProjectItemElement existingPackageReference = project.Items
+                .FirstOrDefault(item => item.ItemType == Constants.PackageReferenceElementName && item.Include.ToLowerInvariant() == packageId.ToLowerInvariant());
+            return existingPackageReference != null;
+        }
+
+        public static ProjectRootElement GetProject(string path)
+        {
+            ProjectRootElement root = null;
+
+            if (File.Exists(path))
+            {
+                var reader = XmlTextReader.Create(new StringReader(File.ReadAllText(path)));
+                root = ProjectRootElement.Create(reader);
+            }
+
+            return root;
+        }
+    }
+}
