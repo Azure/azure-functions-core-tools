5b61f4efd75fca41395e55d967b65ed391adc7d0
Ahmed ElSayed <ahmels@microsoft.com>
2018-11-07 15:50:09 -0800

    Remove x-ms-request-id headder from ARM requests

--- src/Azure.Functions.Cli/Arm/ArmClient.cs
+++ src/Azure.Functions.Cli/Arm/ArmClient.cs
@@ -79,7 +79,6 @@ namespace Azure.Functions.Cli.Arm
                 client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
                 client.DefaultRequestHeaders.Add("User-Agent", "functions-cli/2.0");
                 client.DefaultRequestHeaders.Add("Accept", jsonContentType);
-                client.DefaultRequestHeaders.Add("x-ms-request-id", Guid.NewGuid().ToString());
 
                 HttpResponseMessage response = null;
                 if (StaticSettings.IsDebug)
