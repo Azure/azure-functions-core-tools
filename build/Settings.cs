eac5830f7897f2f5c9f749fe7c22df933fc4779b
Naren Soni <soninaren@users.noreply.github.com>
2018-11-09 12:32:45 -0800

    updating templates to use net sdk version 1.0.24 (#863)

--- build/Settings.cs
+++ build/Settings.cs
@@ -19,8 +19,8 @@ namespace Build
                 : value;
         }
 
-        public const string ItemTemplatesVersion = "2.0.0-10310";
-        public const string ProjectTemplatesVersion = "2.0.0-10310";
+        public const string ItemTemplatesVersion = "2.0.0-10311";
+        public const string ProjectTemplatesVersion = "2.0.0-10311";
 
         public static readonly string SrcProjectPath = Path.GetFullPath("../src/Azure.Functions.Cli/");
 
