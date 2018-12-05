709ace4752e9d7010892c5242a710dc38519d1a6
Naren Soni <soninaren@live.com>
2018-12-04 17:37:53 -0800

    Fixing the build script logic

--- build.ps1
+++ build.ps1
@@ -1,16 +1,29 @@
-#!/usr/bin/env pwsh
-dotnet build Azure.Functions.Cli.sln
 
-$outDir = "$([System.IO.Path]::GetTempPath())cli"
+if ($env:APPVEYOR_REPO_BRANCH -eq "dev") {
+    Set-Location ".\src\Azure.Functions.Cli"
+    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.Functions.JavaWorker -Source  https://ci.appveyor.com/NuGet/azure-functions-java-worker-fejnnsvmrkqg -PreRelease"
+    $javaWorkerVersion = $result.Split()[1]
+    Write-host "Adding Microsoft.Azure.Functions.JavaWorker $javaWorkerVersion to project" -ForegroundColor Green
+    Invoke-Expression -Command "dotnet add package Microsoft.Azure.Functions.JavaWorker -v $javaWorkerVersion -s  https://ci.appveyor.com/NuGet/azure-functions-java-worker-fejnnsvmrkqg"
+    
+    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.Functions.PowerShellWorker -Source https://ci.appveyor.com/nuget/azure-functions-powershell-wor-0842fakagqy6 -PreRelease"
+    $powerShellWorkerVersion = $result.Split()[1]
+    Write-host "Adding Microsoft.Azure.Functions.PowerShellWorker $powerShellWorkerVersion to project" -ForegroundColor Green
+    Invoke-Expression -Command "dotnet add package Microsoft.Azure.Functions.PowerShellWorker -v $powerShellWorkerVersion -s https://ci.appveyor.com/nuget/azure-functions-powershell-wor-0842fakagqy6"
 
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue $outDir
+    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.Functions.NodeJsWorker -Source https://ci.appveyor.com/nuget/azure-functions-nodejs-worker-0fcvx371y52p -PreRelease"
+    $nodeJsWorkerVersion = $result.Split()[1]
+    Write-host "Adding Microsoft.Azure.Functions.NodeJsWorker $nodeJsWorkerVersion to project" -ForegroundColor Green
+    Invoke-Expression -Command "dotnet add package Microsoft.Azure.Functions.NodeJsWorker -v $nodeJsWorkerVersion -s https://ci.appveyor.com/nuget/azure-functions-nodejs-worker-0fcvx371y52p"
 
-if ($IsMacOS) { $runtime = 'osx-x64' }
-elseif ($IsLinux) { $runtime = 'linux-x64' }
-else { $runtime = 'win-x64' }
-
-dotnet publish src/Azure.Functions.Cli/Azure.Functions.Cli.csproj --runtime $runtime --output $outDir
-
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "CLI build succeeded and can be found at: $outDir"
+    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.WebJobs.Script.WebHost -Source https://ci.appveyor.com/NuGet/azure-webjobs-sdk-script-g6rygw981l9t -PreRelease"
+    $WebHostVersion = $result.Split()[1]
+    Write-host "Adding Microsoft.Azure.WebJobs.Script.WebHost $WebHostVersion to project" -ForegroundColor Green
+    Invoke-Expression -Command "dotnet add package Microsoft.Azure.WebJobs.Script.WebHost -v $WebHostVersion -s https://ci.appveyor.com/NuGet/azure-webjobs-sdk-script-g6rygw981l9t"
+    Set-Location "..\..\build"
 }
+else {
+    Set-Location ".\build"
+}
+
+Invoke-Expression -Command  "dotnet run"
