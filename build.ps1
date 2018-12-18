b9a6ae8e04c7f06d87d28b61ed75e3425bcc4de6
Brandon Hurlburt <hurlburb@microsoft.com>
2018-12-18 10:23:12 -0800

    set up POSH build to accept config to enable debuggability

--- build.ps1
+++ build.ps1
@@ -1,4 +1,9 @@
 #!/usr/bin/env pwsh
+param(
+    # The subscription id where the template will be deployed.
+    [Parameter()]
+    [string]$config = "Release"
+)
 dotnet build Azure.Functions.Cli.sln
 
 $outDir = "$([System.IO.Path]::GetTempPath())cli"
@@ -9,7 +14,7 @@ if ($IsMacOS) { $runtime = 'osx-x64' }
 elseif ($IsLinux) { $runtime = 'linux-x64' }
 else { $runtime = 'win-x64' }
 
-dotnet publish src/Azure.Functions.Cli/Azure.Functions.Cli.csproj --runtime $runtime --output $outDir
+dotnet publish src/Azure.Functions.Cli/Azure.Functions.Cli.csproj --runtime $runtime --output $outDir -c $config
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "CLI build succeeded and can be found at: $outDir"
