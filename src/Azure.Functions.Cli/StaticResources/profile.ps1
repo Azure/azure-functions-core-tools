60db9ac973180c3bbbb82b3ed82e7d38f6623761
Tyler James Leonhardt <tylerl0706@gmail.com>
2019-02-19 16:13:39 -0800

    Update to `-Identity`
    which has been fixed in 1.2 of Azure PowerShell

--- src/Azure.Functions.Cli/StaticResources/profile.ps1
+++ src/Azure.Functions.Cli/StaticResources/profile.ps1
@@ -12,9 +12,7 @@
 # Authenticate with Azure PowerShell using MSI.
 # Remove this if you are not planning on using MSI or Azure PowerShell.
 if ($env:MSI_SECRET -and (Get-Module -ListAvailable Az.Accounts)) {
-    $tokenAuthURI = $env:MSI_ENDPOINT + "?resource=https://management.azure.com&api-version=2017-09-01"
-    $tokenResponse = Invoke-RestMethod -Method Get -Headers @{"Secret"="$env:MSI_SECRET"} -Uri $tokenAuthURI
-    Connect-AzAccount -AccessToken $tokenResponse.access_token -AccountId $env:WEBSITE_SITE_NAME
+    Connect-AzAccount -Identity
 }
 
 # Uncomment the next line to enable legacy AzureRm alias in Azure PowerShell.
