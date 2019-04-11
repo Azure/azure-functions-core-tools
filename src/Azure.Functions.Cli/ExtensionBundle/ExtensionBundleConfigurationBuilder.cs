6db4b62facbd59287d37477f4f3aba7ae59ab842
soninaren <soninaren@users.noreply.github.com>
2019-04-11 03:23:53 -0700

    Download and load ExtensionBundle is config is present

---
+++ src/Azure.Functions.Cli/ExtensionBundle/ExtensionBundleConfigurationBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Azure.Functions.Cli.Common;
+using Microsoft.Azure.WebJobs.Script;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Azure.Functions.Cli.ExtensionBundle
+{
+    internal class ExtensionBundleConfigurationBuilder : IConfigureBuilder<IConfigurationBuilder>
+    {
+        private readonly string _scriptPath;
+
+        public ExtensionBundleConfigurationBuilder(string scriptPath)
+        {
+            _scriptPath = scriptPath;
+        }
+
+        public void Configure(IConfigurationBuilder builder)
+        {
+            var bundleId = ExtensionBundleHelper.GetExtensionBundleOptions(_scriptPath).Id;
+            if (!string.IsNullOrEmpty(bundleId))
+            {
+                builder.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "AzureFunctionsJobHost:extensionBundle:downloadPath", Path.Combine(Path.GetTempPath(), "Functions", ScriptConstants.ExtensionBundleDirectory, bundleId)}
+                });
+            }
+
+        }
+    }
+}
---
+++ src/Azure.Functions.Cli/ExtensionBundle/ExtensionBundleHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.WebJobs.Script;
+using Microsoft.Azure.WebJobs.Script.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Functions.Cli.ExtensionBundle
+{
+    class ExtensionBundleHelper
+    {
+        public static ExtensionBundleOptions GetExtensionBundleOptions(string scriptPath)
+        {
+            var hostOptions = new ScriptApplicationHostOptions()
+            {
+                ScriptPath = scriptPath
+            };
+
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.Add(new HostJsonFileConfigurationSource(hostOptions, SystemEnvironment.Instance, loggerFactory: NullLoggerFactory.Instance));
+            var configuration = builder.Build();
+
+            var configurationHelper = new ExtensionBundleConfigurationHelper(configuration, SystemEnvironment.Instance);
+            var options = new ExtensionBundleOptions();
+            configurationHelper.Configure(options);
+            return options;
+        }
+    }
+}
