7f82504b833b03fcf040bad15e7b813e2ffb513f
Ankit Kumar <ankikuma@microsoft.com>
2019-05-01 11:38:44 -0700

    Fixes #1229 - Python version resolutions

--- src/Azure.Functions.Cli/Actions/HostActions/StartHostAction.cs
+++ src/Azure.Functions.Cli/Actions/HostActions/StartHostAction.cs
@@ -249,7 +249,7 @@ namespace Azure.Functions.Cli.Actions.HostActions
         {
             if (workerRuntime == WorkerRuntime.python)
             {
-                await PythonHelpers.VerifyVersion();
+                await PythonHelpers.VerifyPythonVersions(setWorkerExecutable: true);
                 // We need to update the PYTHONPATH to add worker's dependencies
                 var pythonPath = Environment.GetEnvironmentVariable("PYTHONPATH") ?? string.Empty;
                 var pythonWorkerDeps = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "workers", "python", "deps");
