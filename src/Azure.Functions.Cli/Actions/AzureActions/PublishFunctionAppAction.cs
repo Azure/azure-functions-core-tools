b2a3e08200ee8163f02743bb3606181ae23f208d
Ankit Kumar <ankikuma@microsoft.com>
2019-06-03 13:24:16 -0700

    update error message

--- src/Azure.Functions.Cli/Actions/AzureActions/PublishFunctionAppAction.cs
+++ src/Azure.Functions.Cli/Actions/AzureActions/PublishFunctionAppAction.cs
@@ -459,7 +459,6 @@ namespace Azure.Functions.Cli.Actions.AzureActions
 
         public async Task PublishZipDeploy(Site functionApp, Func<Task<Stream>> zipFileFactory)
         {
-            (var content, var length) = CreateStreamContentZip(await zipFileFactory());
             await RetryHelper.Retry(async () =>
             {
                 using (var handler = new ProgressMessageHandler(new HttpClientHandler()))
@@ -470,6 +469,7 @@ namespace Azure.Functions.Cli.Actions.AzureActions
 
                     ColoredConsole.WriteLine("Creating archive for current directory...");
 
+                    (var content, var length) = CreateStreamContentZip(await zipFileFactory());
                     request.Content = content;
 
                     HttpResponseMessage response = null;
