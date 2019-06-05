82252d9f7206313af53f4fa8f3c455e5c2eca6e6
soninaren <soninaren@users.noreply.github.com>
2019-06-05 08:19:19 -0700

    test value

--- build.ps1
+++ build.ps1
@@ -2,6 +2,16 @@ Set-Location ".\build"
 
 Write-Host "dummySecret:$env:dummySecret, non-secret:$env:CLI_DEBUG"
 
+if ($env:dummySecret -eq "dummyValue")
+{
+    Write-Host "accessible"
+}
+else
+{
+    Write-Host "not accessible"
+}
+
+
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
     Invoke-Expression -Command  "dotnet run --sign"
     if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
