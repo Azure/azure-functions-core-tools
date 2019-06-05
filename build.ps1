8d3390e05ab789ffc0ab14f6494f2e42c97508c6
soninaren <soninaren@users.noreply.github.com>
2019-06-04 21:38:19 -0700

    removing nuget restore

--- build.ps1
+++ build.ps1
@@ -1,30 +1,7 @@
+msbuild /version
+dotnet -v
 
-if ($env:APPVEYOR_REPO_BRANCH -eq "dev" -Or $env:APPVEYOR_REPO_BRANCH -eq "devops") {
-    Set-Location ".\src\Azure.Functions.Cli"
-    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.Functions.JavaWorker -Source  https://ci.appveyor.com/NuGet/azure-functions-java-worker-fejnnsvmrkqg -PreRelease"
-    $javaWorkerVersion = $result.Split()[1]
-    Write-host "Adding Microsoft.Azure.Functions.JavaWorker $javaWorkerVersion to project" -ForegroundColor Green
-    Invoke-Expression -Command "dotnet add package Microsoft.Azure.Functions.JavaWorker -v $javaWorkerVersion -s  https://ci.appveyor.com/NuGet/azure-functions-java-worker-fejnnsvmrkqg"
-    
-    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.Functions.PowerShellWorker -Source https://ci.appveyor.com/nuget/azure-functions-powershell-wor-0842fakagqy6 -PreRelease"
-    $powerShellWorkerVersion = $result.Split()[1]
-    Write-host "Adding Microsoft.Azure.Functions.PowerShellWorker $powerShellWorkerVersion to project" -ForegroundColor Green
-    Invoke-Expression -Command "dotnet add package Microsoft.Azure.Functions.PowerShellWorker -v $powerShellWorkerVersion -s https://ci.appveyor.com/nuget/azure-functions-powershell-wor-0842fakagqy6"
-
-    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.Functions.NodeJsWorker -Source https://ci.appveyor.com/nuget/azure-functions-nodejs-worker-0fcvx371y52p -PreRelease"
-    $nodeJsWorkerVersion = $result.Split()[1]
-    Write-host "Adding Microsoft.Azure.Functions.NodeJsWorker $nodeJsWorkerVersion to project" -ForegroundColor Green
-    Invoke-Expression -Command "dotnet add package Microsoft.Azure.Functions.NodeJsWorker -v $nodeJsWorkerVersion -s https://ci.appveyor.com/nuget/azure-functions-nodejs-worker-0fcvx371y52p"
-
-    $result = Invoke-Expression -Command "NuGet list Microsoft.Azure.WebJobs.Script.WebHost -Source https://ci.appveyor.com/NuGet/azure-webjobs-sdk-script-g6rygw981l9t -PreRelease"
-    $WebHostVersion = $result.Split()[1]
-    Write-host "Adding Microsoft.Azure.WebJobs.Script.WebHost $WebHostVersion to project" -ForegroundColor Green
-    Invoke-Expression -Command "dotnet add package Microsoft.Azure.WebJobs.Script.WebHost -v $WebHostVersion -s https://ci.appveyor.com/NuGet/azure-webjobs-sdk-script-g6rygw981l9t"
-    Set-Location "..\..\build"
-}
-else {
-    Set-Location ".\build"
-}
+Set-Location ".\build"
 
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
     Invoke-Expression -Command  "dotnet run --sign"
