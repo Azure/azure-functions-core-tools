3e3820e304b99bfa5ae08e2288e3a58961646208
soninaren <soninaren@users.noreply.github.com>
2019-06-05 16:35:44 -0700

    updating vs template version

--- build/Settings.cs
+++ build/Settings.cs
@@ -20,8 +20,8 @@ namespace Build
                 : value;
         }
 
-        public const string ItemTemplatesVersion = "2.0.10344";
-        public const string ProjectTemplatesVersion = "2.0.10344";
+        public const string ItemTemplatesVersion = "2.0.10364";
+        public const string ProjectTemplatesVersion = "2.0.10364";
 
         public static readonly string SrcProjectPath = Path.GetFullPath("../src/Azure.Functions.Cli/");
 
