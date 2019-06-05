1a70f4a6e731a5f1b70568b2db519932a61ac97e
soninaren <soninaren@users.noreply.github.com>
2019-06-05 00:19:50 -0700

    getting info

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 Set-Location ".\build"
 
+Write-Host "dummySecret:$env:dummySecret, non-secret:$env:CLI_DEBUG"
+
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
     Invoke-Expression -Command  "dotnet run --sign"
     if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
