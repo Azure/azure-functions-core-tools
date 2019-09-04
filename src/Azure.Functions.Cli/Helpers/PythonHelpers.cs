e5f91582be986970126ee28deaa551ac9a85c78d
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-09-04 15:36:34 -0700

    Python worker 1.0.0

--- src/Azure.Functions.Cli/Helpers/PythonHelpers.cs
+++ src/Azure.Functions.Cli/Helpers/PythonHelpers.cs
@@ -14,7 +14,7 @@ namespace Azure.Functions.Cli.Helpers
 {
     public static class PythonHelpers
     {
-        private static readonly string[] _workerPackages = new[] { "azure-functions==1.0.0b5", "azure-functions-worker==1.0.0b11" };
+        private static readonly string[] _workerPackages = new[] { "azure-functions==1.0.3", "azure-functions-worker==1.0.0" };
         private static readonly string _pythonDefaultExecutableVar = "languageWorkers:python:defaultExecutablePath";
         private static bool InVirtualEnvironment => !string.IsNullOrEmpty(VirtualEnvironmentPath);
         public static string VirtualEnvironmentPath => Environment.GetEnvironmentVariable("VIRTUAL_ENV");
