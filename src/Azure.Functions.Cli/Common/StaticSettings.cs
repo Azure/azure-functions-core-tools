f85e3eec863694657723afa56a1b520077013cf2
Ankit Kumar <ankikuma@microsoft.com>
2019-09-18 10:43:42 -0700

    Turn telemetry completely off for now

--- src/Azure.Functions.Cli/Common/StaticSettings.cs
+++ src/Azure.Functions.Cli/Common/StaticSettings.cs
@@ -7,7 +7,9 @@ namespace Azure.Functions.Cli.Common
     {
         public static bool IsDebug => Environment.GetEnvironmentVariable(Constants.CliDebug) == "1";
 
-        public static bool IsTelemetryEnabled => string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.TelemetryOptOutVariable))
-                && Constants.TelemetryInstrumentationKey != "00000000-0000-0000-0000-000000000000";
+        // Turning this off for now
+        // TODO: Enable this once it's ready to be released.
+        public static bool IsTelemetryEnabled => false && (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.TelemetryOptOutVariable))
+                && Constants.TelemetryInstrumentationKey != "00000000-0000-0000-0000-000000000000");
     }
 }
