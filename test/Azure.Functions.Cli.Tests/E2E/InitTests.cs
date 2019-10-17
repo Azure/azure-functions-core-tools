4d3cc519aadfaf5043f3ea3b9c0de348fde0b353
Hanzhang Zeng (Roger) <48038149+Hazhzeng@users.noreply.github.com>
2019-10-17 13:03:29 -0700

    Generate python runtime dockerfile depends on local interpreter… (#1602)
    * Change docker runtime image version depends on local interpreter version
    
    * Add Dockerfile.python36 and Dockerfile.python37

--- test/Azure.Functions.Cli.Tests/E2E/InitTests.cs
+++ test/Azure.Functions.Cli.Tests/E2E/InitTests.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Azure.Functions.Cli.Tests.E2E.Helpers;
 using Xunit;
 using Xunit.Abstractions;
+using Azure.Functions.Cli.Helpers;
+using Azure.Functions.Cli.Common;
 
 namespace Azure.Functions.Cli.Tests.E2E
 {
@@ -131,6 +133,33 @@ namespace Azure.Functions.Cli.Tests.E2E
                     {
                         Name = "Dockerfile",
                         ContentContains = new[] { $"FROM mcr.microsoft.com/azure-functions/{workerRuntime}:2.0" }
+                    }
+                },
+                OutputContains = new[] { "Dockerfile" }
+            }, _output);
+        }
+
+        [SkippableFact]
+        public async Task init_with_python_Dockerfile()
+        {
+            WorkerLanguageVersionInfo worker = await PythonHelpers.ValidatePythonVersion();
+            Skip.If(worker == null);
+
+            string expectedDockerImage = "FROM mcr.microsoft.com/azure-functions/python:2.0";
+            if (worker.Major == 3 && worker.Minor == 7)
+            {
+                expectedDockerImage = "FROM mcr.microsoft.com/azure-functions/python:2.0-python3.7";
+            }
+
+            await CliTester.Run(new RunConfiguration
+            {
+                Commands = new[] { $"init . --worker-runtime python --docker" },
+                CheckFiles = new[]
+                {
+                    new FileResult
+                    {
+                        Name = "Dockerfile",
+                        ContentContains = new[] { expectedDockerImage }
                     }
                 },
                 OutputContains = new[] { "Dockerfile" }
