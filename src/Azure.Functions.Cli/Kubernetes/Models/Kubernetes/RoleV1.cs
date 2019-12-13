2fe397a0964f13d0e79e705e5dca6dcf81bfc59a
Satish Ranjan <satishra@microsoft.com>
2019-12-13 14:42:55 -0800

    Resource insertion index

---
+++ src/Azure.Functions.Cli/Kubernetes/Models/Kubernetes/RoleV1.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Functions.Cli.Kubernetes.Models.Kubernetes
+{
+    public class RoleV1 : BaseKubernetesResource<IKubernetesSpec>
+    {
+        [JsonProperty("rules")]
+        public IEnumerable<RuleV1> Rules { get; set; }
+    }
+
+    public class RuleV1
+    {
+        [JsonProperty("apiGroups")]
+        public IEnumerable<string> ApiGroups { get; set; }
+
+        [JsonProperty("resources")]
+        public IEnumerable<string> Resources { get; set; }
+
+        [JsonProperty("resourceNames")]
+        public IEnumerable<string> ResourceNames { get; set; }
+
+        [JsonProperty("verbs")]
+        public IEnumerable<string> Verbs { get; set; }
+    }
+}
