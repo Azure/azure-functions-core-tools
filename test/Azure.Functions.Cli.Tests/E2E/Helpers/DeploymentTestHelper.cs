ab5190afefa8b8e88f7dd78d6dfbb37b9acb66a1
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-01-03 16:32:31 -0800

    Check with api name

--- test/Azure.Functions.Cli.Tests/E2E/Helpers/DeploymentTestHelper.cs
+++ test/Azure.Functions.Cli.Tests/E2E/Helpers/DeploymentTestHelper.cs
@@ -49,10 +49,16 @@ namespace Azure.Functions.Cli.Tests.E2E.Helpers
             {
                 Commands = new[]
                 {
-                    $"azure functionapp list-functions {appName}"
+                    $"azure functionapp list-functions {appName}",
                 },
-                OutputContains = new string[] { $"    HttpTrigger - [{triggerName}]" },
-                CommandTimeout = TimeSpan.FromMinutes(3)
+                OutputContains = new string[]
+                {
+                    $"api/{triggerName}"
+                },
+                OutputDoesntContain = new string[]
+                {
+                    "api/{triggerName}?code="
+                },
             };
         }
 
