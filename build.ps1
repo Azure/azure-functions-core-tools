0f7eb23073be4be7463bb8b5d06e189d2c671219
Katy Shimizu <kashimiz@microsoft.com>
2020-03-03 15:52:23 -0800

    Revert "Fixed PowerShell build script's MSI generation branch limitation arguments."
    This reverts commit e4bd8fab8764840a796bf890ec97939a6857d100.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
-param([String[]] $MsiGenBranches)
-
-$baseDir = Get-Location
 
 if ($env:APPVEYOR_REPO_BRANCH -eq "disabled") {
     Set-Location ".\src\Azure.Functions.Cli"
@@ -36,55 +33,4 @@ if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
 else {
     Invoke-Expression -Command  "dotnet run"
     if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-}
-
-if ($MsiGenBranches -Contains $env:APPVEYOR_REPO_BRANCH) {
-    Write-Host "Generating MSI files"
-
-    # Add WiX to PATH
-    if (-not (@($env:Path -split ";") -contains $env:WIX))
-    {
-        # Check if the Wix path points to the bin folder
-        if ((Split-Path $env:WIX -Leaf) -ne "bin")
-        {
-            $env:Path += ";$env:WIX\bin"
-        }
-        else
-        {
-            $env:Path += ";$env:WIX"
-        }
-    }
-
-    # Get runtime version
-    $artifactsPath = "$baseDir\artifacts"
-    $buildDir = "$baseDir\build"
-    $cli = Get-ChildItem -Path $artifactsPath -Include func.dll -Recurse | Select-Object -First 1
-    $cliVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($cli).FileVersion
-
-    # Generate MSI installers for Windows
-    @('x64', 'x86') | ForEach-Object { 
-        $platform = $_
-        $targetDir = "$artifactsPath\win-$platform"
-
-        Copy-Item "$buildDir\icon.ico" -Destination $artifactsPath\win-$platform
-        Copy-Item "$buildDir\license.rtf" -Destination $artifactsPath\win-$platform
-        Copy-Item "$buildDir\installbanner.bmp" -Destination $artifactsPath\win-$platform
-        Copy-Item "$buildDir\installdialog.bmp" -Destination $artifactsPath\win-$platform
-        Set-Location $targetDir
-
-        $masterWxsName = "funcinstall"
-        $fragmentName = "$platform-frag"
-        $msiName = "func-cli-$cliVersion-$platform"
-
-        $masterWxsPath = "$buildDir\$masterWxsName.wxs"
-        $fragmentPath = "$buildDir\$fragmentName.wxs"
-        $msiPath = "$artifactsPath\$msiName.msi"
-
-        Invoke-Expression "heat dir '.' -cg FuncHost -dr INSTALLDIR -gg -ke -out $fragmentPath -srd -sreg -template fragment -var var.Source"
-        Invoke-Expression "candle -arch $platform -dPlatform='$platform' -dSource='.' -dProductVersion='$cliVersion' $masterWxsPath $fragmentPath"
-        Invoke-Expression "light -ext WixUIExtension -out $msiPath -sice:ICE61 $masterWxsName.wixobj $fragmentName.wixobj"
-
-        Set-Location $baseDir
-        Get-ChildItem -Path $targetDir -Recurse | Remove-Item -Force -Recurse -ea SilentlyContinue
-    }
 }
