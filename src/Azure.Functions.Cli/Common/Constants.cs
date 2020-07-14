9862f352926c10b5ba9ef618eaccdfcc411251bc
Francisco Gomez Gamino <frangom@microsoft.com>
2020-07-14 16:22:50 -0700

    Set PowerShellWorkerDefaultVersion to '~7'

--- src/Azure.Functions.Cli/Common/Constants.cs
+++ src/Azure.Functions.Cli/Common/Constants.cs
@@ -47,7 +47,7 @@ namespace Azure.Functions.Cli.Common
         public const string AzureManagementAccessToken = "AZURE_MANAGEMENT_ACCESS_TOKEN";
         public const string ExtensionBundleConfigPropertyName = "extensionBundle";
         public const string ManagedDependencyConfigPropertyName = "managedDependency";
-        public const string PowerShellWorkerDefaultVersion = "~6";
+        public const string PowerShellWorkerDefaultVersion = "~7";
 
         public static string CliVersion => typeof(Constants).GetTypeInfo().Assembly.GetName().Version.ToString(3);
 
