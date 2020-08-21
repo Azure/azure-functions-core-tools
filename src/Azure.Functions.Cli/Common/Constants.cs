296bcb0e21f6d896996fbed7be390b67b7d5fc1c
Naren Soni <soninaren@users.noreply.github.com>
2020-08-21 22:03:28 +0000

    changing the default bundle download path

--- src/Azure.Functions.Cli/Common/Constants.cs
+++ src/Azure.Functions.Cli/Common/Constants.cs
@@ -45,6 +45,7 @@ namespace Azure.Functions.Cli.Common
         public const string DefaultManagementURL = "https://management.azure.com/";
         public const string AzureManagementAccessToken = "AZURE_MANAGEMENT_ACCESS_TOKEN";
         public const string ExtensionBundleConfigPropertyName = "extensionBundle";
+        public const string UserCoreToolsDirectory = ".azure-functions-core-tools";
         public const string ManagedDependencyConfigPropertyName = "managedDependency";
         public const string CustomHandlerPropertyName = "customHandler";
         public const string PowerShellWorkerDefaultVersion = "~7";
