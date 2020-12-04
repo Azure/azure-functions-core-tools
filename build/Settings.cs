ecf371145d825b9c7e3b79b8c6b5cd5e8cca4a56
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-12-03 18:10:28 -0800

    Point v3 templates to 3.1.1646

--- build/Settings.cs
+++ build/Settings.cs
@@ -20,9 +20,9 @@ namespace Build
                 : value;
         }
 
-        public const string ItemTemplatesVersion = "3.1.1582";
-        public const string ProjectTemplatesVersion = "3.1.1582";
-        public const string TemplateJsonVersion = "3.1.1582";
+        public const string ItemTemplatesVersion = "3.1.1646";
+        public const string ProjectTemplatesVersion = "3.1.1646";
+        public const string TemplateJsonVersion = "3.1.1646";
 
         public static readonly string SrcProjectPath = Path.GetFullPath("../src/Azure.Functions.Cli/");
 
