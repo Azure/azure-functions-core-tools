f8ba072020128479dd5d25a0b8a0ce93b2a67728
Anatoli Beliaev <anatolib@microsoft.com>
2020-12-21 17:22:40 -0800

    Revert "Fix durable event name in E2E test"
    This reverts commit 4dbdf344a02b08de9b9bd4a90c11a2e27f9b8998.

--- test/Azure.Functions.Cli.Tests/E2E/DurableTests.cs
+++ test/Azure.Functions.Cli.Tests/E2E/DurableTests.cs
@@ -183,7 +183,6 @@ namespace Azure.Functions.Cli.Tests.E2E
 
             string instanceId = $"{Guid.NewGuid():N}";
             string taskHubName = "raiseEventTest";
-            string eventName = "parse";
             DurableHelper.SetTaskHubName(WorkingDirPath, taskHubName);
             Environment.SetEnvironmentVariable(DurableManager.DefaultConnectionStringKey, StorageConnectionString);
 
@@ -192,11 +191,11 @@ namespace Azure.Functions.Cli.Tests.E2E
                 Commands = new[]
                 {
                     $"durable start-new --function-name Counter --input 3 --id {instanceId} --task-hub-name {taskHubName}",
-                    $"durable raise-event --id {instanceId} --event-name {eventName} --event-data add --task-hub-name {taskHubName}"
+                    $"durable raise-event --id {instanceId} --event-name operation --event-data add --task-hub-name {taskHubName}"
                 },
                 OutputContains = new string[]
                 {
-                    $"Raised event '{eventName}' to instance '{instanceId}'"
+                    $"Raised event 'operation' to instance '{instanceId}'"
                 }
             }, 
             _output, 
