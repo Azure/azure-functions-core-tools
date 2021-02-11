1f08bca67655358260be407d5a1ba41ab0b49b3a
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-02-11 07:20:18 +0100

    feat: Change default for installing KEDA
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Azure.Functions.Cli/Actions/KubernetesActions/InstallKedaAction.cs
+++ src/Azure.Functions.Cli/Actions/KubernetesActions/InstallKedaAction.cs
@@ -9,13 +9,13 @@ namespace Azure.Functions.Cli.Actions.KubernetesActions
     [Action(Name = "install", Context = Context.Kubernetes, HelpText = "Install KEDA (non-http scale to zero) in the kubernetes cluster from kubectl config")]
     internal class DeployKedaAction : BaseAction
     {
-        public string Namespace { get; private set; } = "default";
+        public string Namespace { get; private set; }
         public KedaVersion KedaVersion { get; private set; } = KedaVersion.v2;
         public bool DryRun { get; private set; }
 
         public override ICommandLineParserResult ParseArgs(string[] args)
         {
-            SetFlag<string>("namespace", "Kubernetes namespace to deploy to. Default: default", s => Namespace = s);
+            SetFlag<string>("namespace", "Kubernetes namespace to deploy to. Default: Current namespace in Kubernetes config.", ns => Namespace = ns);
             SetFlag<KedaVersion>("keda-version", $"Defines the version of KEDA to install. Default: {KedaVersion.v2}. Options are: {KedaVersion.v1} or {KedaVersion.v2}", f => KedaVersion = f);
             SetFlag<bool>("dry-run", "Show the deployment template", f => DryRun = f);
 
