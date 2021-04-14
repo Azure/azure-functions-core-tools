4b1f3f84e48b0d9bb76d6ee90a5791edf30eaf29
soninaren <nasoni@microsoft.com>
2021-04-14 12:50:12 -0700

    Addding GetBundlePath command

---
+++ src/Azure.Functions.Cli/Actions/LocalActions/GetExtensionBundlePath.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Functions.Cli.Common;
+using Azure.Functions.Cli.ExtensionBundle;
+using Colors.Net;
+using Fclp;
+
+namespace Azure.Functions.Cli.Actions.LocalActions
+{
+    [Action(Name = "GetExtensionBundlePath", ShowInHelp = false)]
+    internal class GetExtensionBundlePath : BaseAction
+    {
+        public string Language { get; set; }
+
+        public override ICommandLineParserResult ParseArgs(string[] args)
+        {
+            return base.ParseArgs(args);
+        }
+
+        public override async Task RunAsync()
+        {
+            var extensionBundleManager = ExtensionBundleHelper.GetExtensionBundleManager();
+            if (extensionBundleManager.IsExtensionBundleConfigured())
+            {
+                try
+                {
+                    string bundlePath = await extensionBundleManager.GetExtensionBundlePath();
+                    if (string.IsNullOrEmpty(bundlePath))
+                    {
+                        throw new CliException("Unable to locate extension bundle.");
+                    }
+                    else
+                    {
+                        ColoredConsole.WriteLine(bundlePath);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new CliException("Unable to locate extension bundle.", e);
+                }
+
+
+            }
+            else
+            {
+                ColoredConsole.WriteLine("Extension bundle not configured.");
+            }
+        }
+    }
+}
