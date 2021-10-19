f960e170427bf8d50433ed31df3bbf7d6218927c
Michael Peng <michaelpeng@microsoft.com>
2021-10-19 15:59:03 -0700

    Updated templates to 4.0.1938

--- build/Settings.cs
+++ build/Settings.cs
@@ -19,10 +19,10 @@ namespace Build
                 : value;
         }
 
-        public const string DotnetIsolatedItemTemplatesVersion = "3.1.1927";
-        public const string DotnetIsolatedProjectTemplatesVersion = "3.1.1927";
-        public const string DotnetItemTemplatesVersion = "4.0.1926";
-        public const string DotnetProjectTemplatesVersion = "4.0.1926";
+        public const string DotnetIsolatedItemTemplatesVersion = "4.0.1938";
+        public const string DotnetIsolatedProjectTemplatesVersion = "4.0.1938";
+        public const string DotnetItemTemplatesVersion = "4.0.1938";
+        public const string DotnetProjectTemplatesVersion = "4.0.1938";
         public const string TemplateJsonVersion = "3.1.1648";
 
         public static readonly string SrcProjectPath = Path.GetFullPath("../src/Azure.Functions.Cli/");
