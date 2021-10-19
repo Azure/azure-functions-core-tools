074a57ce2a6346fdf26456c62ad9142c91de85b7
Will Sugarman <will.sugarman@microsoft.com>
2021-10-19 13:27:11 -0700

    Initial fix

---
+++ src/Azure.Functions.Cli/Kubernetes/Models/DurableTaskConfig.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+
+namespace Azure.Functions.Cli.Kubernetes.Models
+{
+    internal class DurableTaskConfig
+    {
+        [JsonProperty("maxConcurrentOrchestratorFunctions")]
+        public int MaxConcurrentOrchestratorFunctions { get; set; } = 10;
+
+        [JsonProperty("maxConcurrentActivityFunctions")]
+        public int MaxConcurrentActivityFunctions { get; set; } = 1;
+
+        [JsonProperty("storageProvider")]
+        public JObject StorageProvider { get; set; }
+    }
+}
