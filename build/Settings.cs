d6366d4bc4a1593ddbe302763ba0a04b23f0f32e
Michael Peng <mpeng029@gmail.com>
2022-01-13 19:42:46 -0500

    Remove appveyor.yml from V2 (#2900)
    * Removed appveyor references from the build pipeline
    
    * Replaced Invoke-Expression to &{} and corrected an exception's message
    
    * Remove quotes from commands
    
    * Removed quotations from build command
    
    * Altered  so it would run

--- build/Settings.cs
+++ build/Settings.cs
@@ -94,9 +94,9 @@ namespace Build
 
         public static readonly string TelemetryKeyToReplace = "00000000-0000-0000-0000-000000000000";
 
-        public static string BuildNumber => config(null, "devops_buildNumber") ?? config("9999", "APPVEYOR_BUILD_NUMBER");
+        public static string BuildNumber => config("9999", "devops_buildNumber");
 
-        public static string CommitId => config(null, "Build.SourceVersion") ?? config("N/A", "APPVEYOR_REPO_COMMIT");
+        public static string CommitId => config("N/A", "Build.SourceVersion");
 
         public static string TelemetryInstrumentationKey => config(null, "TELEMETRY_INSTRUMENTATION_KEY");
 
