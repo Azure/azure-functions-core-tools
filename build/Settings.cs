e6275578e703bced54c1b33c5b1590ad74d5fc33
Eric Jizba <erijiz@microsoft.com>
2022-02-08 16:54:08 -0800

    update comment

--- build/Settings.cs
+++ build/Settings.cs
@@ -71,7 +71,8 @@ namespace Build
                     { "linux-x64", new [] { "linux", "linux-x64", "unix", "linux-musl-x64" } },
                     { "osx-x64", new [] { "osx", "osx-x64", "unix" } },
                     // NOTE: arm support wasn't added until PowerShell 7.2
-                    // https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-macos?view=powershell-7.2#installation-via-direct-download
+                    // https://docs.microsoft.com/powershell/scripting/install/installing-powershell-on-macos?view=powershell-7.2#installation-via-direct-download
+                    // That being said, we might as well include "osx" and "unix" since it'll hardly affect package size and should lead to more accurate error messages
                     { "osx-arm64", new [] { "osx", "unix" } }
                 }
             },
