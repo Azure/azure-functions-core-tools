90b8178085dfa2e8059bf60fd4f032a7fd7a85ee
Michael Peng <mpeng029@gmail.com>
2022-03-21 14:42:44 -0700

    Make null and unsupported Key Vault reference formats non-blocking (#2991)
    * Make null and unsupported Key Vault references non-blocking
    
    * Changed variable names
    
    * Altered warning message
    
    * Corrected regular expressions and altered warning message
    
    * Remove redundant matching
    
    * Changed regular expression
    
    * Add unit tests and address comments
    
    * Fix unit test for KeyVaultReferenceManager
    
    * Added support for other key vault reference syntax
    
    * Added nested object to test case
    
    * Update test/Azure.Functions.Cli.Tests/KeyVaultReferencesManagerTests.cs
    
    Co-authored-by: Eric Jizba <ejizba@gmail.com>
    
    * Update test/Azure.Functions.Cli.Tests/KeyVaultReferencesManagerTests.cs
    
    Co-authored-by: Eric Jizba <ejizba@gmail.com>
    
    * Update src/Azure.Functions.Cli/Common/KeyVaultReferencesManager.cs
    
    Co-authored-by: Eric Jizba <ejizba@gmail.com>
    
    * Update src/Azure.Functions.Cli/Common/KeyVaultReferencesManager.cs
    
    Co-authored-by: Eric Jizba <ejizba@gmail.com>
    
    Co-authored-by: Eric Jizba <ejizba@gmail.com>

--- src/Azure.Functions.Cli/Common/KeyVaultReferencesManager.cs
+++ src/Azure.Functions.Cli/Common/KeyVaultReferencesManager.cs
@@ -3,17 +3,18 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Colors.Net;
 using Azure.Core;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using static Azure.Functions.Cli.Common.OutputTheme;
 
 namespace Azure.Functions.Cli.Common
 {
     class KeyVaultReferencesManager
     {
         private const string vaultUriSuffix = "vault.azure.net";
-        private static readonly Regex PrimaryKeyVaultReferenceRegex = new Regex(@"^@Microsoft.KeyVault\(SecretUri=(?<VaultUri>[\S^/]+)/(?<Secrets>[\S^/]+)/(?<SecretName>[\S^/]+)/(?<Version>[\S^/]+)\)$", RegexOptions.Compiled);
-        private static readonly Regex SecondaryKeyVaultReferenceRegex = new Regex(@"^@Microsoft.KeyVault\(VaultName=(?<VaultName>[\S^;]+);SecretName=(?<SecretName>[\S^;]+)\)$", RegexOptions.Compiled); 
+        private static readonly Regex BasicKeyVaultReferenceRegex = new Regex(@"^@Microsoft\.KeyVault\((?<ReferenceString>.*)\)$", RegexOptions.Compiled);
         private readonly ConcurrentDictionary<string, SecretClient> clients = new ConcurrentDictionary<string, SecretClient>();
         private readonly TokenCredential credential = new DefaultAzureCredential();
 
@@ -21,17 +22,26 @@ namespace Azure.Functions.Cli.Common
         {
             foreach (var key in settings.Keys.ToList())
             {
-                var keyVaultValue = GetSecretValue(settings[key]);
-                if (keyVaultValue != null)
+                try
                 {
-                    settings[key] = keyVaultValue;
+                    var keyVaultValue = GetSecretValue(key, settings[key]);
+                    if (keyVaultValue != null)
+                    {
+                        settings[key] = keyVaultValue;
+                    }
+                }
+                catch
+                {
+                    // Do not block StartHostAction if secret cannot be resolved: instead, skip it
+                    // and attempt to resolve other secrets
+                    continue;
                 }
             }
         }
 
-        private string GetSecretValue(string value)
+        private string GetSecretValue(string key, string value)
         {
-            var result = ParseSecret(value);
+            var result = ParseSecret(key, value);
 
             if (result != null)
             {
@@ -43,43 +53,78 @@ namespace Azure.Functions.Cli.Common
             return null;
         }
 
-        private ParseSecretResult ParseSecret(string value)
+        internal ParseSecretResult ParseSecret(string key, string value)
         {
-            try
+            // If the value is null, then we return nothing, as the subsequent call to
+            // UpdateEnvironmentVariables(settings) will log to the user that the setting
+            // is skipped. We check here, because Regex.Match throws when supplied with a
+            // null value.
+            if (value == null)
             {
-                return ParsePrimaryRegexSecret(value) ?? ParseSecondaryRegexSecret(value);
+                return null;
             }
-            catch
+            // Determine if the secret value is attempting to use a key vault reference
+            var keyVaultReferenceMatch = BasicKeyVaultReferenceRegex.Match(value);
+            if (keyVaultReferenceMatch.Success)
             {
-                throw new FormatException($"Key Vault Reference format invalid: {value}");
+                var referenceString = keyVaultReferenceMatch.Groups["ReferenceString"].Value;
+                ParseSecretResult result = null;
+                try
+                {
+                    result = ParseVaultReference(referenceString);
+                }
+                catch
+                {
+                    // ignore and show warning below
+                }
+
+                // If we detect that a key vault reference was attempted, but did not match any of
+                // the supported formats, we write a warning to the console.
+                if (result == null)
+                {
+                    ColoredConsole.WriteLine(WarningColor($"Unable to parse the Key Vault reference for setting: {key}"));
+                }
+                return result;
             }
+            return null;
         }
 
-        private ParseSecretResult ParsePrimaryRegexSecret(string value)
+        internal ParseSecretResult ParseVaultReference(string vaultReference)
         {
-            var match = PrimaryKeyVaultReferenceRegex.Match(value);
-            if (match.Success)
+            var secretUriString = GetValueFromVaultReference("SecretUri", vaultReference);
+            if (!string.IsNullOrEmpty(secretUriString))
+            {
+                var secretUri = new Uri(secretUriString);
+                var secretIdentifier = new KeyVaultSecretIdentifier(secretUri);
+                return new ParseSecretResult
+                {
+                    Uri = secretIdentifier.VaultUri,
+                    Name = secretIdentifier.Name,
+                    Version = secretIdentifier.Version
+                };
+            }
+            var vaultName = GetValueFromVaultReference("VaultName", vaultReference);
+            var secretName = GetValueFromVaultReference("SecretName", vaultReference);
+            var version = GetValueFromVaultReference("SecretVersion", vaultReference);
+            if (!string.IsNullOrEmpty(vaultName) && !string.IsNullOrEmpty(secretName))
             {
                 return new ParseSecretResult
                 {
-                    Uri = new Uri(match.Groups["VaultUri"].Value),
-                    Name = match.Groups["SecretName"].Value,
-                    Version = match.Groups["Version"].Value
+                    Uri = new Uri($"https://{vaultName}.{vaultUriSuffix}"),
+                    Name = secretName,
+                    Version = version
                 };
             }
             return null;
         }
 
-        private ParseSecretResult ParseSecondaryRegexSecret(string value)
+        internal string GetValueFromVaultReference(string key, string vaultReference)
         {
-            var altMatch = SecondaryKeyVaultReferenceRegex.Match(value);
-            if (altMatch.Success)
+            var regex = new Regex(key + "=(?<Value>[^;]+)(;|$)");
+            var match = regex.Match(vaultReference);
+            if (match.Success)
             {
-                return new ParseSecretResult
-                {
-                    Uri = new Uri($"https://{altMatch.Groups["VaultName"]}.{vaultUriSuffix}"),
-                    Name = altMatch.Groups["SecretName"].Value
-                };
+                return match.Groups["Value"].Value;
             }
             return null;
         }
@@ -89,7 +134,7 @@ namespace Azure.Functions.Cli.Common
             return clients.GetOrAdd(vaultUri.ToString(), _ => new SecretClient(vaultUri, credential));
         }
 
-        private class ParseSecretResult
+        internal class ParseSecretResult
         {
             public Uri Uri { get; set; }
             public string Name { get; set; }
