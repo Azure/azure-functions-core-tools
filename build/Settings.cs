c88368dae0bb2ad4e53ec848be21c9aa3488acee
Michael Peng <michaelpeng@microsoft.com>
2022-07-05 10:15:57 -0700

    Update isolated templates version

--- build/Settings.cs
+++ build/Settings.cs
@@ -19,8 +19,8 @@ namespace Build
                 : value;
         }
 
-        public const string DotnetIsolatedItemTemplatesVersion = "4.0.2211";
-        public const string DotnetIsolatedProjectTemplatesVersion = "4.0.2211";
+        public const string DotnetIsolatedItemTemplatesVersion = "4.0.2213";
+        public const string DotnetIsolatedProjectTemplatesVersion = "4.0.2213";
         public const string DotnetItemTemplatesVersion = "4.0.2185";
         public const string DotnetProjectTemplatesVersion = "4.0.2185";
         public const string TemplateJsonVersion = "3.1.1648";
