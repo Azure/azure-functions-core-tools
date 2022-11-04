3d765e949733197439e0b33714b789669e9ba05c
Michael Peng <mpeng029@gmail.com>
2022-11-04 11:39:18 -0700

    Altered .csproj path (#3063) (#3196)

--- validateWorkerVersions.ps1
+++ validateWorkerVersions.ps1
@@ -26,7 +26,7 @@ function removeBomIfExists([string]$data)
     return $data
 }
 
-$cliCsprojPath = "./src/Azure.Functions.Cli/Azure.Functions.Cli.csproj"
+$cliCsprojPath = "$PSScriptRoot/src/Azure.Functions.Cli/Azure.Functions.Cli.csproj"
 $cliCsprojContent = removeBomIfExists(Get-Content $cliCsprojPath)
 $cliCsprojXml = [xml]$cliCsprojContent
 
