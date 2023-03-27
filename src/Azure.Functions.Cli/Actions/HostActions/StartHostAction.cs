b1e3211fad60df174f83eb96062938873dd35790
khkh@microsoft.com <khkh@microsoft.com>
2023-03-27 13:43:41 -0500

    Checking the latest version against the current version.

--- src/Azure.Functions.Cli/Actions/HostActions/StartHostAction.cs
+++ src/Azure.Functions.Cli/Actions/HostActions/StartHostAction.cs
@@ -343,6 +343,7 @@ namespace Azure.Functions.Cli.Actions.HostActions
 
         public override async Task RunAsync()
         {
+            var versions = await VersionHelper.GetLatestVersionAsync();
             await PreRunConditions();
 
             var isVerbose = VerboseLogging.HasValue && VerboseLogging.Value;
---
+++ src/Azure.Functions.Cli/Helpers/VersionHelper.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Functions.Cli.Helpers
+{
+    internal class VersionHelper
+    {
+
+        // Download content from https://functionscdn.azureedge.net/public/cli-feed-v4.json and return as string with timeout of two seconds
+        public static async Task<string> GetLatestVersionAsync()
+        {
+            try
+            {
+                var client = new System.Net.Http.HttpClient();
+                client.Timeout = TimeSpan.FromSeconds(5);
+                var response = await client.GetAsync("https://functionscdn.azureedge.net/public/cli-feed-v4.json");
+                var content = await response.Content.ReadAsStringAsync();
+                dynamic data = JsonConvert.DeserializeObject(content);
+                dynamic releases = (IEnumerable) data.releases;
+                return content;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            
+        }
+
+    }
+}
