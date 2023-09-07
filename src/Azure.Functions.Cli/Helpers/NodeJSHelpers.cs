6da35624b3eb0da153614aaaae06abb9e645bcca
khkh@microsoft.com <khkh@microsoft.com>
2023-09-07 14:28:41 -0500

    Changing defaults programming model for node and python.

--- src/Azure.Functions.Cli/Helpers/NodeJSHelpers.cs
+++ src/Azure.Functions.Cli/Helpers/NodeJSHelpers.cs
@@ -7,28 +7,10 @@ namespace Azure.Functions.Cli.Helpers
 {
     public static class NodeJSHelpers
     {
-        public static void PrintV4ReferenceMessage()
-        {
-            ColoredConsole.Write(AdditionalInfoColor("The new Node.js programming model is in public preview. Learn more at "));
-            PrintNodeV4WikiLink();
-        }
-
-        public static void PrintV4AwarenessMessage()
-        {
-            ColoredConsole.Write(AdditionalInfoColor("Did you know? There is a new Node.js programming model in public preview. Learn how you can try it out today at "));
-            PrintNodeV4WikiLink();
-        }
-
-        public static void PrintNodeV4WikiLink()
-        {
-            ColoredConsole.WriteLine(LinksColor("https://aka.ms/AzFuncNodeV4"));
-        }
-
         public static async Task SetupProject(ProgrammingModel programmingModel, string language)
         {
             if (programmingModel == ProgrammingModel.V4)
             {
-                PrintV4ReferenceMessage();
                 if (language == Constants.Languages.TypeScript)
                 {
                     await FileSystemHelpers.WriteFileIfNotExists(Constants.PackageJsonFileName, await StaticResources.PackageJsonTsV4);
@@ -42,7 +24,6 @@ namespace Azure.Functions.Cli.Helpers
             }
             else
             {
-                PrintV4AwarenessMessage();
                 if (language == Constants.Languages.TypeScript)
                 {
                     await FileSystemHelpers.WriteFileIfNotExists(Constants.PackageJsonFileName, await StaticResources.PackageJsonTs);
--- src/Azure.Functions.Cli/Helpers/PythonHelpers.cs
+++ src/Azure.Functions.Cli/Helpers/PythonHelpers.cs
@@ -24,18 +24,6 @@ namespace Azure.Functions.Cli.Helpers
             var pyVersion = await GetEnvironmentPythonVersion();
             AssertPythonVersion(pyVersion, errorIfNoVersion: false);
 
-            // We print a message to the user irrespective of whether they choose the default or preview programming model for
-            // awareness and reference purposes respectively. These messages differ slightly to better indicate which model the
-            // user selected.
-            if (programmingModel == ProgrammingModel.V2)
-            {
-                PrintPySteinReferenceMessage();
-            }
-            else
-            {
-                PrintPySteinAwarenessMessage();
-            }
-
             await CreateRequirements();
             await EnsureVirtualEnvironmentIgnored();
 
@@ -48,23 +36,6 @@ namespace Azure.Functions.Cli.Helpers
             {
                 await CreateGettingStartedMarkdown(programmingModel);
             }
-        }
-
-        public static void PrintPySteinReferenceMessage()
-        {
-            ColoredConsole.Write(AdditionalInfoColor("The new Python programming model is in public preview. Learn more at "));
-            PrintPySteinWikiLink();
-        }
-
-        public static void PrintPySteinAwarenessMessage()
-        {
-            ColoredConsole.Write(AdditionalInfoColor("Did you know? There is a new Python programming model in public preview. For fewer files and a decorator based approach, learn how you can try it out today at "));
-            PrintPySteinWikiLink();
-        }
-
-        public static void PrintPySteinWikiLink()
-        {
-            ColoredConsole.WriteLine(LinksColor("https://aka.ms/pythonprogrammingmodel"));
         }
 
         public static async Task WarnIfAzureFunctionsWorkerInRequirementsTxt()
