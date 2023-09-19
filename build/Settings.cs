3e9a525519f4fae2746a80a79e038a8e5fcdcd09
Khuram Khan <64149947+khkh-ms@users.noreply.github.com>
2023-09-19 11:21:32 -0500

    Adding json.sha256 in the filter (#3459) (#3462)

--- build/Settings.cs
+++ build/Settings.cs
@@ -134,7 +134,7 @@ namespace Build
             public static readonly string ThirdParty = "Sign3rdParty";
             public static readonly string ToThirdPartySign = "ThirdParty";
             public static readonly string[] RuntimesToSign = new[] { "min.win-x86", "min.win-x64" };
-            public static readonly string[] FilterExtenstionsSign = new[] { ".json", ".spec", ".cfg", ".pdb", ".config", ".nupkg", ".py", ".md" };
+            public static readonly string[] FilterExtenstionsSign = new[] { ".json", "json.sha256", ".spec", ".cfg", ".pdb", ".config", ".nupkg", ".py", ".md" };
             public static readonly string SigcheckDownloadURL = "https://functionsbay.blob.core.windows.net/public/tools/sigcheck64.exe";
 
             public static readonly string[] SkipSigcheckTest = new[] {
