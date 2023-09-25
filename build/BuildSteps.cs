092511134486902c1897b895e96cd663979d9549
Khuram Khan <64149947+khkh-ms@users.noreply.github.com>
2023-09-25 13:23:47 -0500

    Using ext bundle instead of hard-coded v2 templates (#3391)
    * Using ext bundle instead of hard-coded v2 templates
    
    * The blueprint append fix.
    
    * minor fix
    
    * Removed an unused variable.
    
    * We don't have fall back templates. Naren confirmed.

--- build/BuildSteps.cs
+++ build/BuildSteps.cs
@@ -286,6 +286,17 @@ namespace Build
                     File.Copy(templatesJsonPath, Path.Combine(Settings.OutputDir, runtime, "templates", "templates.json"));
                 }
             }
+
+            string templatesv2JsonPath = Path.Combine(tempDirectoryPath, "templates-v2", "templates.json");
+            string userPromptsv2JsonPath = Path.Combine(tempDirectoryPath, "bindings-v2", "userPrompts.json");
+            if (File.Exists(templatesv2JsonPath) && File.Exists(userPromptsv2JsonPath))
+            {
+                foreach (var runtime in Settings.TargetRuntimes)
+                {
+                    File.Copy(templatesv2JsonPath, Path.Combine(Settings.OutputDir, runtime, "templates-v2", "templates.json"));
+                    File.Copy(userPromptsv2JsonPath, Path.Combine(Settings.OutputDir, runtime, "templates-v2", "userPrompts.json"));
+                }
+            }
         }
 
         public static void Test()
