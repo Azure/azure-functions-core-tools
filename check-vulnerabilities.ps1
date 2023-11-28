088f37b39c92a6ab0921d9725da966ab8d884f2f
khkh@microsoft.com <khkh@microsoft.com>
2023-11-28 13:36:59 -0600

    build log location fix.

--- check-vulnerabilities.ps1
+++ check-vulnerabilities.ps1
@@ -1,16 +1,16 @@
-dotnet --version
-
-$projectPath = ".\src\Azure.Functions.Cli"
+$projectDirectory = ".\src\Azure.Functions.Cli"
+$projectFileName = ".\Azure.Functions.Cli.csproj"
 $logFilePath = "..\..\build.log"
 if (-not (Test-Path $projectPath))
 {
     throw "Project path '$projectPath' does not exist."
 }
 
-cd ".\src\Azure.Functions.Cli"
+cd $projectDirectory
+dotnet restore $projectFileName
 $cmd = "list", "package", "--include-transitive", "--vulnerable"
 Write-Host "dotnet $cmd"
-dotnet $cmd | Tee-Object build.log
+dotnet $cmd | Tee-Object $logFilePath
 
 $result = Get-content $logFilePath | select-string "has no vulnerable packages given the current sources"
 
