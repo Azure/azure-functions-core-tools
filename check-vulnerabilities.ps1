c87bb7c6165a91d7c6eaa0618db9d3d2de4e90d7
Khuram Khan <64149947+khkh-ms@users.noreply.github.com>
2023-11-28 16:42:33 -0600

    Vulnerability checks in the pipeline (#3536)
    * vulnerability check
    
    * Upgrading Azure.Identity.
    
    * printing result
    
    * bug fix
    
    * Testing something new.
    
    * build log location fix.
    
    * fix again
    
    * removed the debug code.

---
+++ check-vulnerabilities.ps1
@@ -0,0 +1,27 @@
+$projectPath = ".\src\Azure.Functions.Cli"
+$projectFileName = ".\Azure.Functions.Cli.csproj"
+$logFilePath = "..\..\build.log"
+if (-not (Test-Path $projectPath))
+{
+    throw "Project path '$projectPath' does not exist."
+}
+
+cd $projectPath
+dotnet restore $projectFileName
+$cmd = "list", "package", "--include-transitive", "--vulnerable"
+Write-Host "dotnet $cmd"
+dotnet $cmd | Tee-Object $logFilePath
+
+$result = Get-content $logFilePath | select-string "has no vulnerable packages given the current sources"
+
+$logFileExists = Test-Path $logFilePath -PathType Leaf
+if ($logFileExists)
+{
+  Remove-Item $logFilePath
+}
+
+if (!$result)
+{
+  Write-Host "Vulnerabilities found" 
+  Exit 1
+}
--- pipelineUtilities.psm1
+++ pipelineUtilities.psm1
@@ -64,8 +64,8 @@ $DotnetSDKVersionRequirements = @{
     }
 
     '6.0' = @{
-        MinimalPatch = '100'
-        DefaultPatch = '100'
+        MinimalPatch = '417'
+        DefaultPatch = '417'
     }
 }
 
