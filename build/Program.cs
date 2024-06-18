81eb69191cd45d9e974a819aed91c8f9945af636
khkh@microsoft.com <khkh@microsoft.com>
2024-06-18 13:34:11 -0500

    Added the tests back.

--- build/Program.cs
+++ build/Program.cs
@@ -28,7 +28,7 @@ namespace Build
                 .Then(AddGoZip)
                 .Then(TestPreSignedArtifacts, skip: !args.Contains("--ci"))
                 .Then(CopyBinariesToSign, skip: !args.Contains("--ci"))
-                //.Then(Test)
+                .Then(Test)
                 .Then(GenerateSBOMManifestForZips, skip: !args.Contains("--generateSBOM"))
                 .Then(Zip)
                 .Then(DotnetPublishForNupkg)
