55f8b79d32e59ec9a02529f4625149c79b819621
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2024-09-24 12:05:24 -0700

    Core Tools OOP Host (#3802)
    * this doesn't work
    
    * this finally works now
    
    * default should be oop host
    
    * added edge cases
    
    * fixing formatting
    
    * using determineTargetFramework
    
    * saving tests
    
    * adding copy step for OOP
    
    * fixing the webhost reference
    
    * removing test
    
    * setting host version in ps script
    
    * update to validate worker versions script
    
    * update versions for worker packages
    
    * adding ToString
    
    * validating worker version
    
    * validate worker versions
    
    * updating csproj to compile
    
    * addressing pr feedback
    
    * updating build steps
    
    * adding build step
    
    * fixing build step
    
    * trying to get this working
    
    * reverrting target runtimes
    
    * updating tests
    
    * adding dotnet info step
    
    * adding changes
    
    * removing extra test
    
    * trying to specify architecture
    
    * modifying tests to see if they work
    
    * narrowing down to tests that are failing
    
    * trying to see if it works with nobuild flag
    
    * addressing pr feedback
    
    * updating tests with latest logging
    
    * addressing comments and marking flaky tests
    
    * updating so that we are only using net8 framework
    
    * pushing change for branch build
    
    * adding single quotes
    
    * reverting quotes
    
    * adding code mirror fiile
    
    * updating build step
    
    * updatinng build steps
    
    * updating build step
    
    * adding step for dotnet publish
    
    * set inprochost compilation system to diff value and skip flaky test
    
    * updating public build pipeline to trigger
    
    * adding extra changes for pipeline
    
    * public build yml
    
    * updating official build
    
    * readd net8 build artifact step
    
    * readding space back
    
    * addressing initial comments
    
    * adding explicit openTelemetry dlls
    
    * simplifying logic of startHostAction
    
    * addressing PR feedback
    
    * start tests
    
    * changing some of the tests back
    
    * reverting test back to normal
    
    * fixing spacing for csproj
    
    * addressing PR feedback
    
    * adding extra variable
    
    * adding logic for edge case scenarios
    
    * added edge cases tests
    
    * removing extra line in node
    
    * addressing comments
    
    * moving validate host runtime to its own method
    
    * forgot to add return statement

---
+++ src/Azure.Functions.Cli/Common/DotnetConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Functions.Cli.Common
+{
+    internal static class DotnetConstants
+    {
+        public const string WindowsExecutableName = "func.exe";
+        public const string LinuxExecutableName = "func";
+        public const string InProc8DirectoryName = "in-proc8";
+        public const string InProc6DirectoryName = "in-proc6";
+        public const string InProc8HostRuntime = "inproc8";
+        public const string InProc6HostRuntime = "inproc6";
+    }
+}
