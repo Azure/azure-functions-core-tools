b7449a73d4c3a92ab0463663a35dd1776b3a32ba
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2024-09-24 16:46:13 -0700

    Custom host changes for VS Scenario (#3831)
    * this doesn't work
    
    * this finally works now
    
    * default should be oop host
    
    * added edge cases
    
    * fixing formatting
    
    * using determineTargetFramework
    
    * saving tests
    
    * adding copy step for OOP
    
    * fixing the webhost reference
    
    * removing test
    
    * setting host version in ps script
    
    * update to validate worker versions script
    
    * update versions for worker packages
    
    * adding ToString
    
    * validating worker version
    
    * validate worker versions
    
    * updating csproj to compile
    
    * addressing pr feedback
    
    * updating build steps
    
    * adding build step
    
    * fixing build step
    
    * trying to get this working
    
    * reverrting target runtimes
    
    * updating tests
    
    * adding dotnet info step
    
    * adding changes
    
    * removing extra test
    
    * trying to specify architecture
    
    * modifying tests to see if they work
    
    * narrowing down to tests that are failing
    
    * trying to see if it works with nobuild flag
    
    * addressing pr feedback
    
    * updating tests with latest logging
    
    * addressing comments and marking flaky tests
    
    * updating so that we are only using net8 framework
    
    * pushing change for branch build
    
    * adding single quotes
    
    * reverting quotes
    
    * adding code mirror fiile
    
    * updating build step
    
    * updatinng build steps
    
    * updating build step
    
    * adding step for dotnet publish
    
    * set inprochost compilation system to diff value and skip flaky test
    
    * updating public build pipeline to trigger
    
    * adding extra changes for pipeline
    
    * public build yml
    
    * updating official build
    
    * readd net8 build artifact step
    
    * readding space back
    
    * addressing initial comments
    
    * adding explicit openTelemetry dlls
    
    * simplifying logic of startHostAction
    
    * addressing PR feedback
    
    * start tests
    
    * changing some of the tests back
    
    * reverting test back to normal
    
    * fixing spacing for csproj
    
    * addressing PR feedback
    
    * adding extra variable
    
    * adding logic for edge case scenarios
    
    * added edge cases tests
    
    * initial changes
    
    * removing extra line in node
    
    * removing files not needed for custom host
    
    * adding build steps
    
    * addressing comments
    
    * moving validate host runtime to its own method
    
    * forgot to add return statement
    
    * trying out creating directory
    
    * remove skip artifact gen
    
    * updating build step
    
    * updating build to include in official pipeline
    
    * updating publishing custom host step
    
    * code mirror
    
    * trying to get official build working
    
    * program cs update
    
    * updating custom host to work with parsing local.settings.json
    
    * console read line for stalling process
    
    * removing changes not needed for custom host
    
    * only including inproc6 and inproc8 changes
    
    * addressing initial comments
    
    * removing skip artifact gen flag
    
    * verbose logging
    
    * tryna add release pipeline
    
    * windows yaml changes
    
    * addressing comments about yml
    
    * updating log message
    
    * fixing spacing
    
    * fixing spacing again
    
    * trying spacing again
    
    * spacing change
    
    * tryan fix spacing this way
    
    * spacing
    
    * spacing
    
    * new line
    
    * regular indentation
    
    * super indent
    
    * changing spacing back to normal
    
    * addressing comments
    
    * fixing pipeline

---
+++ host/src/CoreToolsHost/Environment/EnvironmentVariables.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace CoreToolsHost;
+
+internal static class EnvironmentVariables
+{
+    /// <summary>
+    /// The worker runtime. Example value: "dotnet-isolated"
+    /// </summary>
+    internal const string FunctionsWorkerRuntime = "FUNCTIONS_WORKER_RUNTIME";
+
+    /// <summary>
+    /// Determines if Functions InProc NET8 is enabled for a .NET 8 in-proc app
+    /// </summary>
+    internal const string FunctionsInProcNet8Enabled = "FUNCTIONS_INPROC_NET8_ENABLED";
+}
