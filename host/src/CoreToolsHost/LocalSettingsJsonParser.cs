65796987bf897ba02b7dcc6f30a6ba7d9f7e4de9
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2024-09-24 16:46:13 -0700

    Custom host changes for VS Scenario (#3831)
    * this doesn't work
    
    * this finally works now
    
    * default should be oop host
    
    * added edge cases
    
    * fixing formatting
    
    * using determineTargetFramework
    
    * saving tests
    
    * adding copy step for OOP
    
    * fixing the webhost reference
    
    * removing test
    
    * setting host version in ps script
    
    * update to validate worker versions script
    
    * update versions for worker packages
    
    * adding ToString
    
    * validating worker version
    
    * validate worker versions
    
    * updating csproj to compile
    
    * addressing pr feedback
    
    * updating build steps
    
    * adding build step
    
    * fixing build step
    
    * trying to get this working
    
    * reverrting target runtimes
    
    * updating tests
    
    * adding dotnet info step
    
    * adding changes
    
    * removing extra test
    
    * trying to specify architecture
    
    * modifying tests to see if they work
    
    * narrowing down to tests that are failing
    
    * trying to see if it works with nobuild flag
    
    * addressing pr feedback
    
    * updating tests with latest logging
    
    * addressing comments and marking flaky tests
    
    * updating so that we are only using net8 framework
    
    * pushing change for branch build
    
    * adding single quotes
    
    * reverting quotes
    
    * adding code mirror fiile
    
    * updating build step
    
    * updatinng build steps
    
    * updating build step
    
    * adding step for dotnet publish
    
    * set inprochost compilation system to diff value and skip flaky test
    
    * updating public build pipeline to trigger
    
    * adding extra changes for pipeline
    
    * public build yml
    
    * updating official build
    
    * readd net8 build artifact step
    
    * readding space back
    
    * addressing initial comments
    
    * adding explicit openTelemetry dlls
    
    * simplifying logic of startHostAction
    
    * addressing PR feedback
    
    * start tests
    
    * changing some of the tests back
    
    * reverting test back to normal
    
    * fixing spacing for csproj
    
    * addressing PR feedback
    
    * adding extra variable
    
    * adding logic for edge case scenarios
    
    * added edge cases tests
    
    * initial changes
    
    * removing extra line in node
    
    * removing files not needed for custom host
    
    * adding build steps
    
    * addressing comments
    
    * moving validate host runtime to its own method
    
    * forgot to add return statement
    
    * trying out creating directory
    
    * remove skip artifact gen
    
    * updating build step
    
    * updating build to include in official pipeline
    
    * updating publishing custom host step
    
    * code mirror
    
    * trying to get official build working
    
    * program cs update
    
    * updating custom host to work with parsing local.settings.json
    
    * console read line for stalling process
    
    * removing changes not needed for custom host
    
    * only including inproc6 and inproc8 changes
    
    * addressing initial comments
    
    * removing skip artifact gen flag
    
    * verbose logging
    
    * tryna add release pipeline
    
    * windows yaml changes
    
    * addressing comments about yml
    
    * updating log message
    
    * fixing spacing
    
    * fixing spacing again
    
    * trying spacing again
    
    * spacing change
    
    * tryan fix spacing this way
    
    * spacing
    
    * spacing
    
    * new line
    
    * regular indentation
    
    * super indent
    
    * changing spacing back to normal
    
    * addressing comments
    
    * fixing pipeline

---
+++ host/src/CoreToolsHost/LocalSettingsJsonParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CoreToolsHost
+{
+    internal static class LocalSettingsJsonParser
+    {
+        internal static async Task<JsonDocument> GetLocalSettingsJsonAsJObjectAsync()
+        {
+            var fullPath = Path.Combine(Environment.CurrentDirectory, "local.settings.json");
+
+            if (File.Exists(fullPath))
+            {
+                string fileContent = "";
+                using (var fileStream = File.Open(fullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    fileContent = await streamReader.ReadToEndAsync();
+                }
+                if (!string.IsNullOrEmpty(fileContent))
+                {
+                    var localSettingsJObject = JsonDocument.Parse(fileContent);
+                    return localSettingsJObject;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ host/src/CoreToolsHost/Program.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+
+using System.Text.Json;
+
+namespace CoreToolsHost
+{
+    internal class Program
+    {
+        static bool isVerbose = false;
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                isVerbose = args.Contains(DotnetConstants.Verbose);
+
+                Logger.LogVerbose(isVerbose, "Starting CoreToolsHost");
+
+                using var appLoader = new AppLoader();
+
+                var localSettingsJson = await LocalSettingsJsonParser.GetLocalSettingsJsonAsJObjectAsync();
+                localSettingsJson.RootElement.TryGetProperty("Values", out JsonElement valuesElement);
+                string workerRuntime = string.Empty;
+
+                if (valuesElement.TryGetProperty(EnvironmentVariables.FunctionsWorkerRuntime, out JsonElement workerRuntimeElement))
+                {
+                    workerRuntime = workerRuntimeElement.GetString();
+                }
+                
+                if (string.IsNullOrEmpty(workerRuntime))
+                {
+                    Logger.Log($"Environment variable '{EnvironmentVariables.FunctionsWorkerRuntime}' is not set.");
+                    return;
+                }
+
+                if (workerRuntime == DotnetConstants.DotnetWorkerRuntime)
+                {
+                    string isInProc8 = "";
+                    if (valuesElement.TryGetProperty(EnvironmentVariables.FunctionsInProcNet8Enabled, out JsonElement inProc8EnabledElement))
+                    {
+                        isInProc8 = inProc8EnabledElement.GetString();
+                    }
+
+                    // Load host assembly for .NET 8 in proc host
+                    if (!string.IsNullOrEmpty(isInProc8) && string.Equals("1", isInProc8))
+                    {
+                        Logger.LogVerbose(isVerbose, "Loading inproc8 host");
+                        LoadHostAssembly(appLoader, args, isNet8InProc: true);
+                    }
+                    else
+                    {
+                        // Load host assembly for .NET 6 in proc host
+                        Logger.LogVerbose(isVerbose, "Loading inproc6 host");
+                        LoadHostAssembly(appLoader, args, isNet8InProc: false);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Log($"An error occurred while running CoreToolsHost.{exception}");
+            }
+        }
+
+        private static void LoadHostAssembly(AppLoader appLoader, string[] args, bool isNet8InProc)
+        {
+            var currentDirectory = AppContext.BaseDirectory;
+            var executableName = DotnetConstants.ExecutableName;
+
+            string filePath = "";
+            filePath = Path.Combine(currentDirectory, isNet8InProc ? DotnetConstants.InProc8DirectoryName: DotnetConstants.InProc6DirectoryName, executableName);
+            Logger.LogVerbose(isVerbose, $"File path to load: {filePath}");
+
+            appLoader.RunApplication(filePath, args);
+        }
+    }
+}
