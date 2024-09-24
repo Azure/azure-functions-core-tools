42f001ac1c840ff91373cfa2949330064f232274
Shyju Krishnankutty <connectshyju@gmail.com>
2024-09-24 16:19:16 -0700

    Adding artifact assembler program which collects artifacts from different sources and assemble the final package for core tools.

---
+++ src/Azure.Functions.ArtifactAssembler/EnvironmentVariables.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Azure.Functions.ArtifactAssembler
+{
+    internal static class EnvironmentVariables
+    {
+        internal const string InProcArtifactAlias = "IN_PROC_ARTIFACT_ALIAS";
+        internal const string CoreToolsHostArtifactAlias = "CORETOOLS_HOST_ARTIFACT_ALIAS";
+        internal const string InProc6ArtifactName = "IN_PROC6_ARTIFACT_NAME";
+        internal const string InProc8ArtifactName = "IN_PROC8_ARTIFACT_NAME";
+        internal const string CoreToolsHostArtifactZipName = "CORETOOLS_HOST_ARTIFACT_NAME";
+    }
+}
+
