3d42b30092c40fe3a7c8ce276f4cdcd9dcf8a9cf
Surgupta <surgupta@microsoft.com>
2024-09-25 15:12:30 -0700

    Adding constants

--- src/Azure.Functions.Cli/Common/DotnetConstants.cs
+++ src/Azure.Functions.Cli/Common/DotnetConstants.cs
@@ -14,5 +14,8 @@ namespace Azure.Functions.Cli.Common
         public const string InProc6DirectoryName = "in-proc6";
         public const string InProc8HostRuntime = "inproc8";
         public const string InProc6HostRuntime = "inproc6";
+        public const string InProcFunctionsSdk = "Microsoft.NET.Sdk.Functions";
+        public const string InProcFunctionsMinSdkVersion = "4.5.0";
+        public const string InProcFunctionsDocsLink = "https://aka.ms/functions-core-tools-in-proc-sdk-requirement";
     }
 }
