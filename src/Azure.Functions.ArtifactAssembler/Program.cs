a408a8b4dd53e9733b7fbc17a63be5e73b073410
Shyju Krishnankutty <connectshyju@gmail.com>
2024-09-26 15:52:38 -0700

    Cleanup

--- src/Azure.Functions.ArtifactAssembler/Program.cs
+++ src/Azure.Functions.ArtifactAssembler/Program.cs
@@ -9,43 +9,7 @@ namespace Azure.Functions.ArtifactAssembler
         {
             try
             {
-                var currentWorkingDirectory = Environment.CurrentDirectory;
-
-                // print child directories present in currentWorkingDirectory.
-                Console.WriteLine("Child directories present in current working directory:");
-                foreach (var directory in Directory.GetDirectories(currentWorkingDirectory))
-                {
-                    Console.WriteLine("  " + directory);
-                    // Print child folder or files in this directory.
-                    foreach (var file in Directory.GetFiles(directory))
-                    {
-                        Console.WriteLine("  -----F " + file);
-                    }
-                    // Print child directories in this directory.
-                    foreach (var childDirectory in Directory.GetDirectories(directory))
-                    {
-                        Console.WriteLine("  -----D " + childDirectory);
-                        if (childDirectory.Contains("drop-coretools-host-windows") || childDirectory.Contains("drop-inproc6"))
-                        {
-                            // print contents of this directory.
-                            foreach (var file in Directory.GetFiles(childDirectory))
-                            {
-                                Console.WriteLine("  ----- ----- F1 " + file);
-                            }
-                            // print child folders
-                            foreach (var subChildDirectory in Directory.GetDirectories(childDirectory))
-                            {
-                                Console.WriteLine("  ----- ----- D1 " + subChildDirectory);
-                                // print contents of this directory.
-                                foreach (var file in Directory.GetFiles(subChildDirectory))
-                                {
-                                    Console.WriteLine("  ----- ----- ----- F2 " + file);
-                                }
-                            }
-                        }
-                    }
-                }
-
+                var currentWorkingDirectory = Environment.CurrentDirectory;             
                 var artifactAssembler = new ArtifactAssembler(currentWorkingDirectory);
                 await artifactAssembler.AssembleArtifactsAsync();
 
