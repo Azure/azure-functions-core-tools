084cacf3c63a6dbe6b2b78dc8c6b86b22f353307
Shyju Krishnankutty <connectshyju@gmail.com>
2024-09-26 19:57:28 -0700

    Adding artifact assembler tool which consolidates the artifacts and produce the package (#3843)
    * Adding artifact assembler program which collects artifacts from different sources and assemble the final package for core tools.
    
    * Adding README
    
    * Remove downloaded artifact directories. Minor language update to README

---
+++ src/Azure.Functions.ArtifactAssembler/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Azure.Functions.ArtifactAssembler
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                var currentWorkingDirectory = Environment.CurrentDirectory;             
+                var artifactAssembler = new ArtifactAssembler(currentWorkingDirectory);
+                await artifactAssembler.AssembleArtifactsAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error running {typeof(Program).FullName} Exception: {ex}");
+                return 1;
+            }
+        }
+    }
+}
