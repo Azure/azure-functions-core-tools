be59f45d0c5b8359cfd255c0719f073c508bafb3
Aishwarya Bhandari <37918412+aishwaryabh@users.noreply.github.com>
2024-10-07 14:05:03 -0700

    Create Artifacts for releasing OOP core tools (#3848)
    * assemble artifacts
    
    * adding code mirror to test
    
    * try awaiting tasks to save space
    
    * removing --net8 dependency
    
    * trying to delete directory one at a time
    
    * trying again with extracting
    
    * diff build steps for vs and cli
    
    * forgot to include other step for var
    
    * trying out new changes
    
    * clean up each zip file after extracting
    
    * trying another approach
    
    * try to delete each directory after copying
    
    * removing logic for creating zips rn
    
    * including e2e test changes and add powershell file
    
    * removing multi targeting
    
    * pass in staging directory
    
    * adding param
    
    * adding logging
    
    * printing out paths
    
    * adding isolated e2e tests and changing verbosity for logger
    
    * adding a new label
    
    * comments
    
    * adding more clarifying comments
    
    * removing extra test and types
    
    * adding logging statement for failing task if we fail tests
    
    * updating tests to only run in winx64 and winx86
    
    * target directory
    
    * target directory
    
    * changes to msi file
    
    * print out directory
    
    * resolving path
    
    * reorg files and try to set env variable
    
    * dont delete artifacts
    
    * update generate msi files
    
    * add doller sign for variable name and fix e2e test
    
    * changing runtime version
    
    * updating to get correct cli version
    
    * lol forgot to have content in zip ps script
    
    * Update src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
    
    Co-authored-by: Lilian Kasem <liliankasem@gmail.com>
    
    * Update src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
    
    Co-authored-by: Lilian Kasem <liliankasem@gmail.com>
    
    * Update src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
    
    Co-authored-by: Lilian Kasem <liliankasem@gmail.com>
    
    * Update src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
    
    Co-authored-by: Lilian Kasem <liliankasem@gmail.com>
    
    * Update src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
    
    Co-authored-by: Lilian Kasem <liliankasem@gmail.com>
    
    * addressing comments
    
    * reverting programcs back to the way it was
    
    * staging directory not correct
    
    * merging
    
    * Revert "merging"
    
    This reverts commit 89cc110d6789dee7a51597d76cb16b859338b126.
    
    * fixing spacing
    
    * wrong directory
    
    * trying to free up disk space
    
    * removing custom code mirror
    
    * removing dollar signs
    
    * updating build number
    
    * adding variables back
    
    * fix build number
    
    * fixing candle
    
    * fixing msi files
    
    * reverting files back to normal
    
    ---------
    
    Co-authored-by: Lilian Kasem <liliankasem@gmail.com>

---
+++ src/Azure.Functions.ArtifactAssembler/PipelineHelpers/generateMsiFiles.ps1
@@ -0,0 +1,85 @@
+param (
+    [string]$ArtifactsPath
+)
+
+$baseDir = Get-Location
+
+Write-Host "Generating MSI files"
+
+# Add WiX to PATH
+if (-not (@($env:Path -split ";") -contains $env:WIX))
+{
+    # Check if the Wix path points to the bin folder
+    if ((Split-Path $env:WIX -Leaf) -ne "bin")
+    {
+        $env:Path += ";$env:WIX\bin"
+    }
+    else
+    {
+        $env:Path += ";$env:WIX"
+    }
+}
+
+# Get runtime version
+$buildDir = "$baseDir\..\..\build"
+Write-Host "Build directory: $buildDir"
+$cli = Get-ChildItem -Path $ArtifactsPath -Include func.dll -Recurse |
+    Where-Object { 
+        # Get the parent directory of func.dll
+        $parentDir = Split-Path $_.FullName -Parent
+
+        # Ensure that the func.dll is not inside inproc6 or inproc8
+        (Split-Path $parentDir -Parent) -eq $ArtifactsPath
+    } |
+    Select-Object -First 1 # Only get the first matching func.dll
+$cliVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($cli).FileVersion
+$buildNumberForZipFile = ($cliVersion -split "\.")[2]
+Write-Host "Build number: $buildNumberForZipFile"
+Write-Host "##vso[task.setvariable variable=BuildNumberForZipFile;]$buildNumberForZipFile"
+
+# Define the platforms to search for
+$platforms = @('x64', 'x86')
+
+# Generate MSI installers for Windows
+# TODO: add 'arm64' to the below array once a production-ready version of the WiX toolset supporting
+# it is released. See https://github.com/Azure/azure-functions-core-tools/issues/3122
+Get-ChildItem -Path $ArtifactsPath | ForEach-Object {
+    # Check if the subdirectory name includes 'win-x64 or win-x86'
+    $subDir = $_.FullName
+    $matchedPlatform = $null
+    foreach ($platform in $platforms) {
+        if ($subDir -like "*Cli.win-$platform*") {
+            $matchedPlatform = $platform
+            break  # Exit loop once a platform match is found
+        }
+    }
+    # If a matching platform was found
+    if ($matchedPlatform) {
+        $targetDir = $subDir
+        Write-Host "Target directory: $targetDir"
+
+        Copy-Item "$buildDir\icon.ico" -Destination $targetDir
+        Copy-Item "$buildDir\license.rtf" -Destination $targetDir
+        Copy-Item "$buildDir\installbanner.bmp" -Destination $targetDir
+        Copy-Item "$buildDir\installdialog.bmp" -Destination $targetDir
+        Set-Location $targetDir
+
+        $masterWxsName = "funcinstall"
+        $fragmentName = "$matchedPlatform-frag"
+        $msiName = "func-cli-$cliVersion-$matchedPlatform"
+
+        $masterWxsPath = "$buildDir\$masterWxsName.wxs"
+        $fragmentPath = "$buildDir\$fragmentName.wxs"
+        $msiPath = "$artifactsPath\$msiName.msi"
+
+        & { heat dir '.' -cg FuncHost -dr INSTALLDIR -gg -ke -out $fragmentPath -srd -sreg -template fragment -var var.Source }
+        & { candle -arch $matchedPlatform -dPlatform="$matchedPlatform" -dSource='.' -dProductVersion="$cliVersion" $masterWxsPath $fragmentPath }
+        & { light -ext "WixUIExtension" -out $msiPath -sice:"ICE61" "$masterWxsName.wixobj" "$fragmentName.wixobj" }
+    
+        # Check that the .msi files are actually present
+        if (-not(Test-Path -Path $msiPath))
+        {
+            throw "$msiPath not found."
+        }
+    }
+}
