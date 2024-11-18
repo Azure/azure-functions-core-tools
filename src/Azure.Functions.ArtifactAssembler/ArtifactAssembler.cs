5fceed1ed79b7b7c716fd4f917d3dea17c40cc49
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-11-18 15:19:09 -0800

    removing zipping for now

--- src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
+++ src/Azure.Functions.ArtifactAssembler/ArtifactAssembler.cs
@@ -188,9 +188,9 @@ namespace Azure.Functions.ArtifactAssembler
                 await Task.WhenAll(inProc8CopyTask, inProc6CopyTask, coreToolsHostCopyTask);
 
                 // consolidatedArtifactDirPath now contains custom core-tools host, in-proc6 and in-proc8 sub directories. Create a zip file.
-                var zipPath = Path.Combine(customHostTargetArtifactDir, $"{consolidatedArtifactDirName}.zip");
-                await Task.Run(() => FileUtilities.CreateZipFile(consolidatedArtifactDirPath, zipPath));
-                Console.WriteLine($"Successfully created target runtime zip at: {zipPath}");
+                //var zipPath = Path.Combine(customHostTargetArtifactDir, $"{consolidatedArtifactDirName}.zip");
+                //await Task.Run(() => FileUtilities.CreateZipFile(consolidatedArtifactDirPath, zipPath));
+                //Console.WriteLine($"Successfully created target runtime zip at: {zipPath}");
 
                 Directory.Delete(consolidatedArtifactDirPath, true);
             });
