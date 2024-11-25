ad23dc68d1a3146e365d60d5136f2e0e6801be87
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-11-25 11:01:22 -0800

    updating test project

--- test/Azure.Functions.Cli.Tests/E2E/Helpers/TestTraits.cs
+++ test/Azure.Functions.Cli.Tests/E2E/Helpers/TestTraits.cs
@@ -27,8 +27,8 @@ namespace Azure.Functions.Cli.Tests.E2E.Helpers
         public const string UseInConsolidatedArtifactGeneration = "UseInConsolidatedArtifactGeneration";
 
         /// <summary>
-        /// Tests with UseInConsolidatedArtifactGeneration label will be used in the default scenario and in the artifact consolidation pipeline
-        /// We still want to run these tests in the PR/ official core tools pipelines and in the artifact consolidation pipeline for a sanity check before publishing the artifacts.
+        /// Tests with UseInVisualStudioConsolidatedArtifactGeneration label will not be run in the default scenario and only in the artifact consolidation pipeline
+        /// Otherwise tests with this label will fail in the PR/ official core tools pipelines since the nested inproc artifacts are not present.
         /// </summary>
         public const string UseInVisualStudioConsolidatedArtifactGeneration = "UseInVisualStudioConsolidatedArtifactGeneration";
     }
