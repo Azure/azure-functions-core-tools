96150e13e857486812c627d81c9e420c278ae401
Aishwarya Bhandari <aibhandari@microsoft.com>
2024-11-25 13:13:21 -0800

    removing not needed logging

--- host/src/CoreToolsHost/AppLoader/AppLoader.cs
+++ host/src/CoreToolsHost/AppLoader/AppLoader.cs
@@ -32,22 +32,6 @@ namespace CoreToolsHost
                     assembly_path = GetCharArrayPointer(assemblyPath)
                 };
 
-                // Convert to IntPtr first
-                IntPtr ptr = new IntPtr(parameters.assembly_path);
-
-                // Convert IntPtr to string (ANSI)
-                string paramAssemblyPath = Marshal.PtrToStringAnsi(ptr);
-                Console.WriteLine($"Value of assembly path: {paramAssemblyPath}");
-                Console.WriteLine("Does assembly path exist: " + File.Exists(paramAssemblyPath));
-
-                IntPtr dotnetRootPtr = new IntPtr(parameters.dotnet_root);
-
-                string paramAssemblyPathDotnetRoot = Marshal.PtrToStringAnsi(dotnetRootPtr);
-                Console.WriteLine($"Value of dotnetRoot: {paramAssemblyPathDotnetRoot}");
-                Console.WriteLine("Does dotnetRoot exist: " + File.Exists(paramAssemblyPathDotnetRoot));
-
-                Console.WriteLine($"Size of params: {parameters.size}");
-
                 isVerbose = commandLineArgs.Contains(DotnetConstants.Verbose);
 
                 var hostfxrFullPath = NetHost.GetHostFxrPath(&parameters);
