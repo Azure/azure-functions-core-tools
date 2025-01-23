25cfcc064f21d493d10c70d2cf6caccb3193e569
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-01-22 16:46:47 -0800

    seeing if this works

---
+++ build/AzureLoginTask.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Build
+{
+    public class AzureLoginTask : Task
+    {
+        [Required]
+        public string DirectoryId { get; set; }
+
+        [Required]
+        public string AppId { get; set; }
+
+        [Required]
+        public string Key { get; set; }
+
+        public override bool Execute()
+        {
+            if (string.IsNullOrEmpty(DirectoryId) ||
+                string.IsNullOrEmpty(AppId) ||
+                string.IsNullOrEmpty(Key))
+            {
+                Log.LogMessage(MessageImportance.High, "Skipping Azure login - credentials not provided");
+                return false;
+            }
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Shell.Run("cmd", $"/c az login --service-principal -u {DirectoryId} -p \"{Key}\" --tenant {DirectoryId}", silent: true);
+            }
+            else
+            {
+                Shell.Run("az", $"login --service-principal -u {AppId} -p \"{Key}\" --tenant {DirectoryId}", silent: true);
+            }
+            return true;
+        }
+    }
+}
