12ac0352a32645b6f3faa63941e02b8c35a6b758
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-03-11 11:46:49 -0700

    addressing comments; lets see if this compiles lol

---
+++ src/Azure.Functions.ArtifactAssembler/PipelineHelpers/generateSha.ps1
@@ -0,0 +1,9 @@
+$rootPath = "$(Pipeline.Workspace)\staging"
+$zipFiles = Get-ChildItem -Path $rootPath -Filter *.zip  -Recurse
+foreach ($file in $zipfiles) 
+{
+	$sha = (Get-FileHash $file.FullName).Hash.ToLower()
+	$shaFilePath = $file.FullName + ".sha2"
+	Out-File -InputObject $sha -Encoding ascii -FilePath $shaFilePath -NoNewline
+	Write-Host "Generated sha for $filePath at $shaFilePath"
+}
---
+++ src/Azure.Functions.ArtifactAssembler/PipelineHelpers/moveNugetPackage.ps1
@@ -0,0 +1,35 @@
+﻿# Retrieve environment variables
+$defaultArtifactAlias = $env:DEFAULT_ARTIFACT_ALIAS
+$defaultArtifactName = $env:DEFAULT_ARTIFACT_NAME
+
+# Get the current working directory
+$currentDirectory = "$(Pipeline.Workspace)"
+
+# Construct the path using current directory, and environment variables
+$artifactPath = Join-Path -Path $currentDirectory -ChildPath "$defaultArtifactAlias\$defaultArtifactName"
+
+# Define the regex pattern to match the nupkg file (digit.digit.4digits)
+$regexPattern = "^Microsoft\.Azure\.Functions\.CoreTools\.\d+\.\d+\.\d{4}\.nupkg$"
+
+# Look for the first nupkg file that matches the pattern in the constructed path
+$fileToMove = Get-ChildItem -Path $artifactPath -Filter "*.nupkg" | Where-Object { $_.Name -match $regexPattern } | Select-Object -First 1
+
+# Check if a matching file was found
+if ($fileToMove -eq $null) {
+    Write-Host "No .nupkg file matching the pattern was found in $artifactPath"
+    exit 1
+}
+
+# Define the destination path in $(Pipeline.Workspace)/nugetPackage
+$nugetPackageDirectory = Join-Path -Path $(Pipeline.Workspace) -ChildPath "nugetPackage"
+
+# Create the nugetPackage directory if it doesn't exist
+if (-not (Test-Path $nugetPackageDirectory)) {
+    New-Item -Path $nugetPackageDirectory -ItemType Directory
+    Write-Host "Directory created at: $nugetPackageDirectory"
+}
+
+# Move the file to the staging directory
+Move-Item -Path $fileToMove.FullName -Destination $nugetPackageDirectory -Force
+
+Write-Host "File $($fileToMove.Name) moved to $nugetPackageDirectory"
