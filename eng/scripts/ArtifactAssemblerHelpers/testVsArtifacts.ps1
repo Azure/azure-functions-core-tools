57bfae25841be02e645c9cd751a03c51bdb7712a
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-03-27 14:26:48 -0700

    fixing vs file

--- eng/scripts/ArtifactAssemblerHelpers/testVsArtifacts.ps1
+++ eng/scripts/ArtifactAssemblerHelpers/testVsArtifacts.ps1
@@ -3,9 +3,13 @@ param (
     [string]$StagingDirectory
 )
 
+$rootDir = Join-Path $PSScriptRoot "../../.." | Resolve-Path
+Write-Host "$rootDir"
+ls $rootDir
+
 # Set the path to test project (.csproj) and runtime settings
-$testProjectPath = "..\..\test\Azure.Functions.Cli.Tests\Azure.Functions.Cli.Tests.csproj"
-$runtimeSettings = "..\..\test\Azure.Functions.Cli.Tests\E2E\StartTests_artifact_consolidation_visualstudio.runsettings"
+$testProjectPath = ".\test\Azure.Functions.Cli.Tests\Azure.Functions.Cli.Tests.csproj"
+$runtimeSettings = ".\test\Azure.Functions.Cli.Tests\E2E\StartTests_artifact_consolidation_visualstudio.runsettings"
 
 [System.Environment]::SetEnvironmentVariable("FUNCTIONS_WORKER_RUNTIME", "dotnet", "Process")
 
