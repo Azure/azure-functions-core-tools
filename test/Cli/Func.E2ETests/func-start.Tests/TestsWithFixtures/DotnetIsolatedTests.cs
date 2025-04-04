ab014ff2e06024e9ca50336857744eccb1e0f92e
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-04-04 16:54:08 -0700

    only have one test wait for the host bro

--- test/Cli/Func.E2ETests/func-start.Tests/TestsWithFixtures/DotnetIsolatedTests.cs
+++ test/Cli/Func.E2ETests/func-start.Tests/TestsWithFixtures/DotnetIsolatedTests.cs
@@ -55,6 +55,7 @@ namespace Func.E2ETests.func_start.Tests.TestsWithFixtures
 
             funcStartCommand.ProcessStartedHandler = async (process, fileWriter)=>
             {
+                await Task.Delay(5000);
                 capturedContent = await ProcessHelper.ProcessStartedHandlerHelper(port, process, fileWriter, "HttpTrigger");
             };
 
