5b1ab2450d7e1f777a3a3e357fb8ff25e775afdc
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-04-04 14:40:09 -0700

    forgot to comment out nodev3 tests

--- test/Cli/Func.E2ETests/func-start.Tests/TestsWithFixtures/NodeV3Tests.cs
+++ test/Cli/Func.E2ETests/func-start.Tests/TestsWithFixtures/NodeV3Tests.cs
@@ -1,42 +1,42 @@
-﻿using FluentAssertions;
-using Func.E2ETests.Fixtures;
-using Func.TestFramework.Assertions;
-using Func.TestFramework.Commands;
-using Func.TestFramework.Helpers;
-using Xunit.Abstractions;
-using Xunit;
+﻿//using FluentAssertions;
+//using Func.E2ETests.Fixtures;
+//using Func.TestFramework.Assertions;
+//using Func.TestFramework.Commands;
+//using Func.TestFramework.Helpers;
+//using Xunit.Abstractions;
+//using Xunit;
 
-namespace Func.E2ETests.func_start.Tests.TestsWithFixtures
-{
-    [Collection("NodeV3")]
-    public class NodeV3Tests : IClassFixture<NodeV3FunctionAppFixture>
-    {
-        private readonly NodeV3FunctionAppFixture _fixture;
-        public NodeV3Tests(NodeV3FunctionAppFixture fixture, ITestOutputHelper log)
-        {
-            _fixture = fixture;
-            _fixture.Log = log;
-        }
+//namespace Func.E2ETests.func_start.Tests.TestsWithFixtures
+//{
+//    [Collection("NodeV3")]
+//    public class NodeV3Tests : IClassFixture<NodeV3FunctionAppFixture>
+//    {
+//        private readonly NodeV3FunctionAppFixture _fixture;
+//        public NodeV3Tests(NodeV3FunctionAppFixture fixture, ITestOutputHelper log)
+//        {
+//            _fixture = fixture;
+//            _fixture.Log = log;
+//        }
 
-        [Fact]
-        public async Task Start_NodeJsApp_V3_SuccessfulFunctionExecution()
-        {
-            int port = ProcessHelper.GetAvailablePort();
-            // Call func start
-            var funcStartCommand = new FuncStartCommand(_fixture.FuncPath, _fixture.Log, "Start_NodeJsApp_V3_SuccessfulFunctionExecution");
+//        [Fact]
+//        public async Task Start_NodeJsApp_V3_SuccessfulFunctionExecution()
+//        {
+//            int port = ProcessHelper.GetAvailablePort();
+//            // Call func start
+//            var funcStartCommand = new FuncStartCommand(_fixture.FuncPath, _fixture.Log, "Start_NodeJsApp_V3_SuccessfulFunctionExecution");
 
-            string capturedContent = null;
+//            string capturedContent = null;
 
-            funcStartCommand.ProcessStartedHandler = async (process, fileWriter) =>
-            {
-                capturedContent = await ProcessHelper.ProcessStartedHandlerHelper(port, process, fileWriter, "HttpTrigger");
-            };
+//            funcStartCommand.ProcessStartedHandler = async (process, fileWriter) =>
+//            {
+//                capturedContent = await ProcessHelper.ProcessStartedHandlerHelper(port, process, fileWriter, "HttpTrigger");
+//            };
 
-            var result = funcStartCommand
-                        .WithWorkingDirectory(_fixture.WorkingDirectory)
-                        .Execute(new[] { "--verbose", "--port", port.ToString() });
+//            var result = funcStartCommand
+//                        .WithWorkingDirectory(_fixture.WorkingDirectory)
+//                        .Execute(new[] { "--verbose", "--port", port.ToString() });
 
-            capturedContent.Should().Be("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.");
-        }
-    }
-}
+//            capturedContent.Should().Be("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.");
+//        }
+//    }
+//}
