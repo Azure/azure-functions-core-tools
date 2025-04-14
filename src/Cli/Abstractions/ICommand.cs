9f3503cd979924f064b543ec0eb3dc81dad72a98
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-04-14 10:44:15 -0700

    creating abstractions

---
+++ src/Cli/Abstractions/ICommand.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Note that this file is copied from: https://github.com/dotnet/sdk
+// Once the dotnet cli utils package is in a published consumable state, we will migrate over to use that
+
+namespace Azure.Functions.Cli.Abstractions
+{
+    public interface ICommand
+    {
+        CommandResult Execute();
+
+        ICommand WorkingDirectory(string projectDirectory);
+
+        ICommand EnvironmentVariable(string name, string? value);
+
+        ICommand CaptureStdOut();
+
+        ICommand CaptureStdErr();
+
+        ICommand ForwardStdOut(TextWriter? to = null, bool onlyIfVerbose = false, bool ansiPassThrough = true);
+
+        ICommand ForwardStdErr(TextWriter? to = null, bool onlyIfVerbose = false, bool ansiPassThrough = true);
+
+        ICommand OnOutputLine(Action<string> handler);
+
+        ICommand OnErrorLine(Action<string> handler);
+
+        ICommand SetCommandArgs(string commandArgs);
+
+        string CommandName { get; }
+
+        string CommandArgs { get; }
+    }
+}
---
+++ src/Cli/Abstractions/IEnvironmentProvider.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Note that this file is copied from: https://github.com/dotnet/sdk
+// Once the dotnet cli utils package is in a published consumable state, we will migrate over to use that
+namespace Azure.Functions.Cli.Abstractions
+{
+    public interface IEnvironmentProvider
+    {
+        IEnumerable<string> ExecutableExtensions { get; }
+
+        string? GetCommandPath(string commandName, params string[] extensions);
+
+        string? GetCommandPathFromRootPath(string rootPath, string commandName, params string[] extensions);
+
+        string? GetCommandPathFromRootPath(string rootPath, string commandName, IEnumerable<string> extensions);
+
+        bool GetEnvironmentVariableAsBool(string name, bool defaultValue);
+
+        int? GetEnvironmentVariableAsNullableInt(string name);
+
+        string? GetEnvironmentVariable(string name);
+
+        string? GetEnvironmentVariable(string variable, EnvironmentVariableTarget target);
+
+        void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target);
+    }
+
+}
