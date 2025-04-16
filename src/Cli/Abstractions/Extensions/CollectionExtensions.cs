bb13151ede4ccf461ff653842d7666f40c69d4e6
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-04-16 13:28:47 -0700

    fixing license comment

--- src/Cli/Abstractions/Extensions/CollectionExtensions.cs
+++ src/Cli/Abstractions/Extensions/CollectionExtensions.cs
@@ -1,5 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 // Copied from: https://github.com/dotnet/sdk/blob/4a81a96a9f1bd661592975c8269e078f6e3f18c9/src/Cli/Microsoft.DotNet.Cli.Utils/Extensions/CollectionsExtensions.cs
 
