f6d21dd4cd4fbc340553dbb080f1078525839250
Lilian Kasem <likasem@microsoft.com>
2025-04-16 16:40:38 -0700

    Misc

--- src/Cli/func/Diagnostics/DisableConsoleConfigurationBuilder.cs
+++ src/Cli/func/Diagnostics/DisableConsoleConfigurationBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using Microsoft.Azure.WebJobs.Script;
 using Microsoft.Extensions.Configuration;
 
