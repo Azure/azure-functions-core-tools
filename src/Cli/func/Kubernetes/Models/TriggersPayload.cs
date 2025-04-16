58456ce4208913db594bfe9a3b93bcb751c5fb76
Lilian Kasem <likasem@microsoft.com>
2025-04-16 16:43:24 -0700

    Fix kubernetes

--- src/Cli/func/Kubernetes/Models/TriggersPayload.cs
+++ src/Cli/func/Kubernetes/Models/TriggersPayload.cs
@@ -1,4 +1,6 @@
-using System.Collections.Generic;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
