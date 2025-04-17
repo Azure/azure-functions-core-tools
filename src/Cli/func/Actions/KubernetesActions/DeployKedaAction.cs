eef8383d64c7b112c4937861fc3d7c3cb8dcf202
Lilian Kasem <likasem@microsoft.com>
2025-04-16 17:08:05 -0700

    Fix actions part 2

--- src/Cli/func/Actions/KubernetesActions/DeployKedaAction.cs
+++ src/Cli/func/Actions/KubernetesActions/DeployKedaAction.cs
@@ -1,4 +1,6 @@
-using System.Threading.Tasks;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using Azure.Functions.Cli.Kubernetes;
 using Azure.Functions.Cli.Kubernetes.KEDA;
 using Colors.Net;
@@ -10,19 +12,21 @@ namespace Azure.Functions.Cli.Actions.KubernetesActions
     internal class DeployKedaAction : BaseAction
     {
         public string Namespace { get; private set; }
-        public KedaVersion KedaVersion { get; private set; } = KedaVersion.v2;
+
+        public KedaVersion KedaVersion { get; private set; } = KedaVersion.V2;
+
         public bool DryRun { get; private set; }
 
         public override ICommandLineParserResult ParseArgs(string[] args)
         {
             SetFlag<string>("namespace", "Kubernetes namespace to deploy to. Default: Current namespace in Kubernetes config.", ns => Namespace = ns);
-            SetFlag<KedaVersion>("keda-version", $"Defines the version of KEDA to install. Default: {KedaVersion.v2}. Options are: {KedaVersion.v1} or {KedaVersion.v2}", f => KedaVersion = f);
+            SetFlag<KedaVersion>("keda-version", $"Defines the version of KEDA to install. Default: {KedaVersion.V2}. Options are: {KedaVersion.V1} or {KedaVersion.V2}", f => KedaVersion = f);
             SetFlag<bool>("dry-run", "Show the deployment template", f => DryRun = f);
 
             return base.ParseArgs(args);
         }
 
-        public async override Task RunAsync()
+        public override async Task RunAsync()
         {
             var kedaDeploymentYaml = KedaHelper.GetKedaDeploymentYaml(Namespace, KedaVersion);
             if (DryRun)
@@ -31,9 +35,9 @@ namespace Azure.Functions.Cli.Actions.KubernetesActions
             }
             else
             {
-                if (!await KubernetesHelper.NamespaceExists(Namespace))
+                if (!await KubernetesHelpers.NamespaceExists(Namespace))
                 {
-                    await KubernetesHelper.CreateNamespace(Namespace);
+                    await KubernetesHelpers.CreateNamespace(Namespace);
                 }
 
                 ColoredConsole.WriteLine($"Installing KEDA {KedaVersion} in namespace {Namespace}");
