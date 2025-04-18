1537ca2bcc0d2276b3392a2299c8fcb1927a1a24
Dirk Roeleveld <dirkroel@gmail.com>
2025-04-18 13:44:25 +0000

    Fix: func new not creating a valid template with Python

--- src/Cli/func/Actions/LocalActions/CreateFunctionAction.cs
+++ src/Cli/func/Actions/LocalActions/CreateFunctionAction.cs
@@ -159,7 +159,7 @@ namespace Azure.Functions.Cli.Actions.LocalActions
                 var providedInputs = new Dictionary<string, string>()
                 {
                     { GetFunctionNameParamId, FunctionName },
-                    { HttpTriggerAuthLevelParamId, AuthorizationLevel?.ToString().ToUpper() }
+                    { HttpTriggerAuthLevelParamId, AuthorizationLevel?.ToString().ToUpperInvariant() }
                 };
 
                 var jobName = "appendToFile";
