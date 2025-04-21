22e8118f83b8e10bf1d909f5dbfef0ed35bbc39b
Lilian Kasem <likasem@microsoft.com>
2025-04-21 10:24:38 -0700

    Fix style in abstractions project (#4383)

--- src/Cli/Abstractions/Environment/IEnvironmentProvider.cs
+++ src/Cli/Abstractions/Environment/IEnvironmentProvider.cs
@@ -1,27 +1,27 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 // Copied from: https://github.com/dotnet/sdk/blob/4a81a96a9f1bd661592975c8269e078f6e3f18c9/src/Cli/Microsoft.DotNet.Cli.Utils/IEnvironmentProvider.cs
-namespace Azure.Functions.Cli.Abstractions.Environment
+namespace Azure.Functions.Cli.Abstractions
 {
     public interface IEnvironmentProvider
     {
-        IEnumerable<string> ExecutableExtensions { get; }
+        public IEnumerable<string> ExecutableExtensions { get; }
 
-        string? GetCommandPath(string commandName, params string[] extensions);
+        public string? GetCommandPath(string commandName, params string[] extensions);
 
-        string? GetCommandPathFromRootPath(string rootPath, string commandName, params string[] extensions);
+        public string? GetCommandPathFromRootPath(string rootPath, string commandName, params string[] extensions);
 
-        string? GetCommandPathFromRootPath(string rootPath, string commandName, IEnumerable<string> extensions);
+        public string? GetCommandPathFromRootPath(string rootPath, string commandName, IEnumerable<string> extensions);
 
-        bool GetEnvironmentVariableAsBool(string name, bool defaultValue);
+        public bool GetEnvironmentVariableAsBool(string name, bool defaultValue);
 
-        int? GetEnvironmentVariableAsNullableInt(string name);
+        public int? GetEnvironmentVariableAsNullableInt(string name);
 
-        string? GetEnvironmentVariable(string name);
+        public string? GetEnvironmentVariable(string name);
 
-        string? GetEnvironmentVariable(string variable, EnvironmentVariableTarget target);
+        public string? GetEnvironmentVariable(string variable, EnvironmentVariableTarget target);
 
-        void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target);
+        public void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target);
     }
 }
--- src/Cli/Abstractions/Logging/IReporter.cs
+++ src/Cli/Abstractions/Logging/IReporter.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 // Copied from: https://github.com/dotnet/sdk/blob/4a81a96a9f1bd661592975c8269e078f6e3f18c9/src/Cli/Microsoft.DotNet.Cli.Utils/IReporter.cs
-namespace Azure.Functions.Cli.Abstractions.Logging
+namespace Azure.Functions.Cli.Abstractions
 {
     public interface IReporter
     {
-        void WriteLine(string message);
+        public void WriteLine(string message);
 
-        void WriteLine();
+        public void WriteLine();
 
-        void WriteLine(string format, params object?[] args);
+        public void WriteLine(string format, params object?[] args);
 
-        void Write(string message);
+        public void Write(string message);
     }
 }
