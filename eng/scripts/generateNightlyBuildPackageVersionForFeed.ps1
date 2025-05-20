3f9fbde9d5aa906ad909553afb5986b4df084a10
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-05-20 11:35:03 -0700

    trying out changes with default pipeline

--- eng/scripts/generateNightlyBuildPackageVersionForFeed.ps1
+++ eng/scripts/generateNightlyBuildPackageVersionForFeed.ps1
@@ -1,16 +1,16 @@
-# Generate a SemVer 2.0 compliant version number
-# Format: 4.0.YYYYMMDD (where YYYYMMDD has no leading zeros)
-$year = (Get-Date).Year
-$month = (Get-Date).Month
-$day = (Get-Date).Day
+$cliVersion = $env:cliVersion
 
-# Construct date as an integer (no leading zeros)
-$dateInt = ($year * 10000) + ($month * 100) + $day
+# Throw an error if cliVersion is not found
+if ([string]::IsNullOrEmpty($cliVersion)) {
+    throw "Error: cliVersion variable not found. Make sure it is set in a previous step in the same job."
+}
 
-# Create version in format 4.0.YYYYMMDD
-#$uniqueVersion = "4.0.$dateInt"
-$uniqueVersion = $env:BUILD_BUILDID
-Write-Host "Generated unique version for nightly build: $uniqueVersion"
+# Get build ID from environment variable or use timestamp if not available
+$buildId = $env:BUILD_BUILDID
 
-# Set as pipeline variable for use in the Universal Packages task
-Write-Host "##vso[task.setvariable variable=NightlyBuildVersion]$uniqueVersion"
+# Create SemVer 2.0 compliant version: cliVersion+buildId
+$semVerVersion = "$cliVersion+$buildId"
+Write-Host "Generated SemVer 2.0 version: $semVerVersion"
+
+# Set as pipeline variables for use in subsequent tasks
+Write-Host "##vso[task.setvariable variable=NightlyBuildVersion]$semVerVersion"
