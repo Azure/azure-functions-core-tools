f5656468beec6549fd86dd560779d90736681900
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-05-22 16:21:39 -0700

    fixing not needed changes and * for all downloaded artifacts

--- eng/scripts/ArtifactAssemblerHelpers/generateMsiFiles.ps1
+++ eng/scripts/ArtifactAssemblerHelpers/generateMsiFiles.ps1
@@ -48,7 +48,6 @@ $cliVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($cli).FileVer
 $buildNumberForZipFile = ($cliVersion -split "\.")[2]
 Write-Host "Build number: $buildNumberForZipFile"
 Write-Host "##vso[task.setvariable variable=BuildNumberForZipFile;]$buildNumberForZipFile"
-Write-Host "##vso[task.setvariable variable=cliVersion;]$cliVersion"
 
 # Define the platforms to search for
 $platforms = @('x64', 'x86')
