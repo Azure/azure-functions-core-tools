fc668ea4d4ebc70fae80bad4fd14f1cf2ea471c7
v-umangsr <v-umangsr@DESKTOP-VK97IEP>
2025-05-27 17:50:25 +0530

    Removed template error

--- test/Cli/Func.E2E.Tests/Commands/FuncNew/InvalidInputsTests.cs
+++ test/Cli/Func.E2E.Tests/Commands/FuncNew/InvalidInputsTests.cs
@@ -14,43 +14,7 @@ using Xunit.Abstractions;
 
 namespace Azure.Functions.Cli.E2E.Tests.Commands.FuncNew
 {
-    [Trait(TestTraits.WorkerRuntime, WorkerRuntimeTraits.Node)]
-    public class InvalidInputsTests(ITestOutputHelper log) : BaseE2ETests(log)
+    public class InvalidInputsTests
     {
-        [Fact]
-        public void FuncNew_InvalidTemplate_ShowsError()
-        {
-            var testName = nameof(FuncNew_InvalidTemplate_ShowsError);
-
-            // Init a function app with node
-            new FuncInitCommand(FuncPath, testName, Log)
-                .WithWorkingDirectory(WorkingDirectory)
-                .Execute([".", "--worker-runtime", "node"]);
-
-            // Try creating a function with an invalid template
-            var result = new FuncNewCommand(FuncPath, testName, Log)
-                .WithWorkingDirectory(WorkingDirectory)
-                .Execute([".", "--template", "invalidTemplate", "--name", "testfunc"]);
-
-            result.Should().HaveStdErrContaining("Can't find template \"invalidTemplate\" in \"javascript\"");
-        }
-
-        [Fact]
-        public void FuncNew_InvalidAuthLevel_ShowsError()
-        {
-            var testName = nameof(FuncNew_InvalidAuthLevel_ShowsError);
-
-            // Init with node
-            new FuncInitCommand(FuncPath, testName, Log)
-                .WithWorkingDirectory(WorkingDirectory)
-                .Execute([".", "--worker-runtime", "node"]);
-
-            // Try creating function with invalid auth level
-            var result = new FuncNewCommand(FuncPath, testName, Log)
-                .WithWorkingDirectory(WorkingDirectory)
-                .Execute([".", "--template", "httpTrigger", "--name", "testfunc", "--authlevel", "superadmin"]);
-
-            result.Should().HaveStdOutContaining("Authorization level is applicable to templates that use Http trigger, Allowed values: [function, anonymous, admin]");
-        }
     }
 }
