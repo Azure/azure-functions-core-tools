8c1136ab671794ccde641d244ee3604831addb9b
Lilian Kasem <likasem@microsoft.com>
2025-05-27 13:52:27 -0700

    Fix linux package DEB script & CI pipeline (#4440)

--- eng/tools/publish-tools/shared/helper.py
+++ eng/tools/publish-tools/shared/helper.py
@@ -88,18 +88,23 @@ def linuxOutput(buildFolder, arch):
     exeFullPath = os.path.abspath("func")
 
     os.chdir(buildFolder)
+
     # strip sharedobjects
     import glob
 
-    sharedObjects = glob.glob("**/*.so", recursive=True)
+    stripBinary = "strip"
+    if arch == "arm64":
+        stripBinary = "aarch64-linux-gnu-strip"
 
     # obj files inside the workers should not be removed as workers like "python"
     # come with objects necessary for the worker to work.
+    sharedObjects = glob.glob("**/*.so", recursive=True)
     sharedObjects = [obj for obj in sharedObjects if "workers" not in obj]
-    printReturnOutput(["strip", "--strip-unneeded"] + sharedObjects)
 
-    chmodFolderAndFiles(os.path.join(buildFolder, "usr"))
+    printReturnOutput([stripBinary, "--strip-unneeded"] + sharedObjects)
+
     print(f"change bin/func permission to 755")
+    chmodFolderAndFiles(os.path.join(buildFolder, "usr"))
     # octal
     os.chmod(exeFullPath, 0o755)
 
