824554d65c298549d2956205d9fa9e5a2e0499c4
Lilian Kasem <likasem@microsoft.com>
2025-06-02 17:33:20 -0700

    Setup official pipeline signing

--- eng/scripts/publish-cli-all-rids.ps1
+++ eng/scripts/publish-cli-all-rids.ps1
@@ -1,10 +1,22 @@
 param (
   [string]$projectFile = "src\Cli\func\Azure.Functions.Cli.csproj",
-  [string]$outputDirRoot = "artifacts"
+  [string]$outputDirRoot = "artifacts",
+  [string]$zip = $false,
+  [string]$zipOutputDirRoot = "artifacts-zipped",
+  [switch]$noBuild = $false
 )
 
-# List of runtimes
-$runtimes = Get-Content -Path "$PSScriptRoot/data/supported-runtimes.txt"
+$runtimes = @(
+  "min.win-arm64",
+  "min.win-x86",
+  "min.win-x64",
+  "linux-x64",
+  "osx-x64",
+  "osx-arm64",
+  "win-x86",
+  "win-x64",
+  "win-arm64"
+)
 
 foreach ($runtime in $runtimes) {
   Write-Host "Publishing runtime: $runtime"
@@ -24,8 +36,16 @@ foreach ($runtime in $runtimes) {
     "-r", $rid
     "--self-contained"
     "-o", $outputPath
-    # "-p:ZipAfterPublish=true"
   )
+
+  if ($noBuild -eq $true) {
+    $commandArgs += "--no-build"
+  }
+
+  if ($zip -eq $true) {
+    $commandArgs += "/p:ZipAfterPublish=true"
+    $commandArgs += "/p:ZipArtifactsPath=$zipOutputDirRoot"
+  }
 
   # Add IsMinified property if runtime starts with min.
   if ($runtime.StartsWith("min.")) {
