d81c82d9567e51b30596d1119ba454346cb26a3e
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-06-05 14:37:22 -0700

    fixing syntax again

--- test/Cli/TestFramework/Commands/FuncPackCommand.cs
+++ test/Cli/TestFramework/Commands/FuncPackCommand.cs
@@ -1,10 +1,6 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Azure.Functions.Cli.TestFramework;
 using Xunit.Abstractions;
 
 namespace Azure.Functions.Cli.TestFramework.Commands
