f8d582734fcfcbd0ba82d74351e8d9a1e32c12d4
Lilian Kasem <likasem@microsoft.com>
2025-06-10 16:30:47 -0700

    Update versioning logic & move TelemetryInstrumentationKey to its own Constants file (#4469)
    * Update versioning logic & move TelemetryInstrumentationKey
    
    * Use attribute approach for setting TelemetryInstrumentationKey

---
+++ src/Cli/func/Common/Constants.Build.cs
@@ -0,0 +1,20 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Reflection;
+
+namespace Azure.Functions.Cli.Common
+{
+    internal static partial class Constants
+    {
+        public static readonly string TelemetryInstrumentationKey =
+            typeof(Constants).Assembly.GetCustomAttribute<TelemetryInstrumentationKeyAttribute>().Value
+            ?? "00000000-0000-0000-0000-000000000000";
+    }
+
+    [AttributeUsage(AttributeTargets.Assembly)]
+    internal class TelemetryInstrumentationKeyAttribute(string value) : Attribute
+    {
+        public string Value => value;
+    }
+}
