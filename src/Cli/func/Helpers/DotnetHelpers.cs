3560c05917b254d43e2dd3672f553e5bde601bb0
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-06-13 13:24:08 -0700

    trying without retry

--- src/Cli/func/Helpers/DotnetHelpers.cs
+++ src/Cli/func/Helpers/DotnetHelpers.cs
@@ -297,9 +297,10 @@ namespace Azure.Functions.Cli.Helpers
             {
                 await UninstallWebJobsTemplates();
                 await InstallIsolatedTemplates();
+                await Task.Delay(10000); // Allow some time for the templates to be installed
 
                 // Wait for templates to be available before proceeding
-                await WaitForTemplateAvailability("func", TimeSpan.FromSeconds(30));
+                // await WaitForTemplateAvailability("func", TimeSpan.FromSeconds(30));
                 await action();
             }
             finally
