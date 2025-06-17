14b07b7d8336156681c20b0da804c2e893af0deb
v-vreyya <v-vreyya@DESKTOP-RT7L5HG>
2025-06-17 16:15:48 +0530

    func durable tests

---
+++ test/Cli/Func.E2E.Tests/Fixtures/DurableFunctionAppFixture.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Azure.Functions.Cli.E2E.Tests.Fixtures
+{
+    public class DurableFunctionAppFixture : BaseDurableFunctionAppFixture
+    {
+        public DurableFunctionAppFixture()
+            : base()
+        {
+        }
+    }
+}
