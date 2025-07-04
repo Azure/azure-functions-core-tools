d40e403ebd02f3e64d749015dd79ba6fa3637090
v-vreyya <v-vreyya@DESKTOP-RT7L5HG>
2025-07-04 21:04:33 +0530

    considering fallback

--- src/Azure.Functions.Cli/Program.cs
+++ src/Azure.Functions.Cli/Program.cs
@@ -14,7 +14,7 @@ namespace Azure.Functions.Cli
         internal static void Main(string[] args)
         {
             // Set console encoding to UTF-8 to properly display international characters
-            Console.OutputEncoding = Encoding.UTF8;
+            SetConsoleEncoding();
 
             FirstTimeCliExperience();
             SetupGlobalExceptionHandler();
@@ -99,6 +99,20 @@ namespace Azure.Functions.Cli
                 .As<IContextHelpManager>();
 
             return builder.Build();
+        }
+
+        private static void SetConsoleEncoding()
+        {
+            // Set console encoding to UTF-8 to properly display international characters
+            try
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+            }
+            catch
+            {
+                // Silently fall back to default encoding if UTF-8 isn't supported
+                // International characters may not display correctly but CLI will still function
+            }
         }
     }
 }
