cfd8116f8aa804819db0f20b32d7c2c6ef6dd4c5
umangsriv <v-umangsr@microsoft.com>
2025-07-14 21:29:20 +0530

    Migrate func new E2E tests to the new test project (#4452)
    Co-authored-by: v-umangsr <v-umangsr@DESKTOP-VK97IEP>
    Co-authored-by: v-vreyya <v-vreyya@DESKTOP-RT7L5HG>

--- test/Cli/Func.E2E.Tests/BaseE2ETests.cs
+++ test/Cli/Func.E2E.Tests/BaseE2ETests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Runtime.InteropServices;
+using Azure.Functions.Cli.Abstractions;
 using Azure.Functions.Cli.TestFramework.Helpers;
 using Xunit;
 using Xunit.Abstractions;
