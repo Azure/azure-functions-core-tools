43c803ca2439e51acdb94b4ea70e023f18a5e50e
Lilian Kasem <likasem@microsoft.com>
2025-07-16 23:13:54 -0700

    Fix python tests

--- test/Cli/Func.E2ETests/Commands/FuncExtensions/ExtensionBundleTestsBase.cs
+++ test/Cli/Func.E2ETests/Commands/FuncExtensions/ExtensionBundleTestsBase.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Azure.Functions.Cli.TestFramework.Assertions;
@@ -11,68 +11,75 @@ namespace Azure.Functions.Cli.E2ETests.Commands.FuncExtensions
 {
     public class ExtensionBundleTestsBase(ITestOutputHelper log) : BaseE2ETests(log)
     {
-        public async Task ExtensionsInstallBundlesConfiguredByDefault(string worker, string templateName, string testName)
+        public async Task ExtensionsInstallBundlesConfiguredByDefault(string worker, string templateName, string testName, params string[] additionalInitArgs)
         {
-            var workingDir = WorkingDirectory;
-            await FuncInitWithRetryAsync(testName, [".", "--worker-runtime", worker]);
+            var initArgs = new[] { ".", "--worker-runtime", worker }.Concat(additionalInitArgs ?? Array.Empty<string>()).ToArray();
+
+            await FuncInitWithRetryAsync(testName, initArgs);
             await FuncNewWithRetryAsync(testName, ["--template", templateName, "--name", "testfunc"]);
+
             var result = new FuncRootCommand(FuncPath, testName, Log)
-                            .WithWorkingDirectory(workingDir)
+                            .WithWorkingDirectory(WorkingDirectory)
                             .Execute(["extensions", "install"]);
 
             result.Should().ExitWith(0);
             result.Should().HaveStdOutContaining("No action performed. Extension bundle is configured");
         }
 
-        public async Task ExtensionsInstallNoBundleArgWithExtVersion(string worker, string templateName, string testName)
+        public async Task ExtensionsInstallNoBundleArgWithExtVersion(string worker, string templateName, string testName, params string[] additionalInitArgs)
         {
-            var workingDir = WorkingDirectory;
-            await FuncInitWithRetryAsync(testName, [".", "--worker-runtime", worker, "--no-bundle"]);
+            var initArgs = new[] { ".", "--worker-runtime", worker, "--no-bundle" }.Concat(additionalInitArgs ?? Array.Empty<string>()).ToArray();
+
+            await FuncInitWithRetryAsync(testName, initArgs);
             await FuncNewWithRetryAsync(testName, ["--template", templateName, "--name", "testfunc"]);
+
             var result = new FuncRootCommand(FuncPath, testName, Log)
-                            .WithWorkingDirectory(workingDir)
+                            .WithWorkingDirectory(WorkingDirectory)
                             .Execute(["extensions", "install", "-p", "Microsoft.Azure.WebJobs.Extensions.Storage", "-v", "5.3.0"]);
 
             result.Should().ExitWith(0);
             result.Should().HaveStdOutContaining("Restoring packages for");
-            result.Should().HaveStdOutContaining("Installed Microsoft.Azure.WebJobs.Extensions.Storage 5.3.0");
+            result.Should().HaveStdOutContaining("Microsoft.Azure.WebJobs.Extensions.Storage");
         }
 
-        public async Task ExtensionsInstallNoBundleArgWithNotValidTrigger(string worker, string templateName, string testName)
+        public async Task ExtensionsInstallNoBundleArgWithNotValidTrigger(string worker, string templateName, string testName, params string[] additionalInitArgs)
         {
-            var workingDir = WorkingDirectory;
-            await FuncInitWithRetryAsync(testName, [".", "--worker-runtime", worker, "--no-bundle"]);
+            var initArgs = new[] { ".", "--worker-runtime", worker, "--no-bundle" }.Concat(additionalInitArgs ?? Array.Empty<string>()).ToArray();
+
+            await FuncInitWithRetryAsync(testName, initArgs);
             await FuncNewWithRetryAsync(testName, ["--template", templateName, "--name", "testfunc"]);
+
             var result = new FuncRootCommand(FuncPath, testName, Log)
-                            .WithWorkingDirectory(workingDir)
+                            .WithWorkingDirectory(WorkingDirectory)
                             .Execute(["extensions", "install"]);
 
             result.Should().ExitWith(0);
             result.Should().HaveStdOutContaining("No action performed because no functions in your app require extensions");
         }
 
-        public async Task GetExtensionBundlePathReturnsBundlePath(string worker, string testName)
+        public async Task GetExtensionBundlePathReturnsBundlePath(string worker, string testName, params string[] additionalInitArgs)
         {
-            var workingDir = WorkingDirectory;
+            var initArgs = new[] { ".", "--worker-runtime", worker }.Concat(additionalInitArgs ?? Array.Empty<string>()).ToArray();
             string bundlePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), Common.Constants.UserCoreToolsDirectory, "Functions", ScriptConstants.ExtensionBundleDirectory);
 
-            await FuncInitWithRetryAsync(testName, [".", "--worker-runtime", worker]);
+            await FuncInitWithRetryAsync(testName, initArgs);
+
             var result = new FuncRootCommand(FuncPath, testName, Log)
-                            .WithWorkingDirectory(workingDir)
+                            .WithWorkingDirectory(WorkingDirectory)
                             .Execute(["GetExtensionBundlePath"]);
 
             result.Should().ExitWith(0);
             result.Should().HaveStdOutContaining(bundlePath);
         }
 
-        public async Task GetExtensionBundlePathNoBundleArgBundlesNotConfiigured(string worker, string testName)
+        public async Task GetExtensionBundlePathNoBundleArgBundlesNotConfiigured(string worker, string testName, params string[] additionalInitArgs)
         {
-            var workingDir = WorkingDirectory;
-            string bundlePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), Common.Constants.UserCoreToolsDirectory, "Functions", ScriptConstants.ExtensionBundleDirectory);
+            var initArgs = new[] { ".", "--worker-runtime", worker, "--no-bundle" }.Concat(additionalInitArgs ?? Array.Empty<string>()).ToArray();
 
-            await FuncInitWithRetryAsync(testName, [".", "--worker-runtime", worker, "--no-bundle"]);
+            await FuncInitWithRetryAsync(testName, initArgs);
+
             var result = new FuncRootCommand(FuncPath, testName, Log)
-                            .WithWorkingDirectory(workingDir)
+                            .WithWorkingDirectory(WorkingDirectory)
                             .Execute(["GetExtensionBundlePath"]);
 
             result.Should().ExitWith(0);
