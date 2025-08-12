a974fcd5d9086cc64b4f040f21a06228f0eb5b41
Shyju Krishnankutty <connectshyju@gmail.com>
2025-08-12 13:48:10 -0700

    Save files with UTF-8 encoding

--- src/Cli/func/Common/TargetFramework.cs
+++ src/Cli/func/Common/TargetFramework.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 namespace Azure.Functions.Cli.Common
