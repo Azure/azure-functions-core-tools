e8e26c6ae3d323457ab721412a0a466d2cbed5c2
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-08-13 10:58:26 -0700

    addressing comments for dotnet subcommand

--- src/Cli/func/Actions/LocalActions/PackAction/PackOptions.cs
+++ src/Cli/func/Actions/LocalActions/PackAction/PackOptions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-
 namespace Azure.Functions.Cli.Actions.LocalActions.PackAction
 {
     public class PackOptions
