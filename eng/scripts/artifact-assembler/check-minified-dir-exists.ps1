5c11db8f1051db8c4d15d57311f345081ce77d80
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-08-19 12:35:22 -0700

    updating check to see if worker dir is empty

--- eng/scripts/artifact-assembler/check-minified-dir-exists.ps1
+++ eng/scripts/artifact-assembler/check-minified-dir-exists.ps1
@@ -1,6 +1,11 @@
 $workersPath = "$(Pipeline.Workspace)/minified-test/workers"
 
-if (Test-Path $workersPath) {
-Write-Error "Unexpected workers directory found at $workersPath. Minified builds should not include language workers."
-} else {
-Write-Host "Workers directory correctly absent for minified build."
+if (-Not (Test-Path $workersPath)) {
+    Write-Error "Workers directory is missing at $workersPath. It must exist in minified builds."
+}
+
+$contents = Get-ChildItem -Path $workersPath
+
+if ($contents.Count -ne 0) {
+    Write-Error "Workers directory is not empty. Minified builds should not include language workers."
+}
