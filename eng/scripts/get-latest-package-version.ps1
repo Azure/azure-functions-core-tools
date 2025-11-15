6d6226bdeba614ca03c2545a16225c00e7551235
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-11-14 16:32:28 -0800

    addressing comments

---
+++ eng/scripts/get-latest-package-version.ps1
@@ -0,0 +1,25 @@
+function Get-LatestPackageVersion {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$packageName,
+        
+        [Parameter(Mandatory = $true)]
+        [hashtable]$headers
+    )
+  
+    $feedUrl = "https://feeds.dev.azure.com/azfunc/internal/_apis/packaging/feeds/core-tools-nightly-build/packages"
+    $apiVersion = "7.1"
+
+    $searchUrl = "$feedUrl?packageNameQuery=$packageName&api-version=$apiVersion"
+    $searchResponse = Invoke-RestMethod -Uri $searchUrl -Headers $headers -Method Get
+    $package = $searchResponse.value | Where-Object { $_.name -eq $packageName } | Select-Object -First 1
+    
+    if (!$package) {
+        Write-Warning "Package '$packageName' not found in the feed."
+        return $null
+    }
+    
+    $packageUrl = "$feedUrl/$($package.id)?api-version=$apiVersion"
+    $packageDetails = Invoke-RestMethod -Uri $packageUrl -Headers $headers -Method Get
+    return $packageDetails.versions[0].version
+}
