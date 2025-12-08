b8a75aceb7d9a393adf8dfd89854ffcad9578e61
Victoria Hall <victoriahall@microsoft.com>
2025-12-08 14:13:57 -0600

    Fix syntax

--- src/Cli/func/Actions/AzureActions/PublishFunctionAppAction.cs
+++ src/Cli/func/Actions/AzureActions/PublishFunctionAppAction.cs
@@ -824,11 +824,12 @@ namespace Azure.Functions.Cli.Actions.AzureActions
 
             if (PublishBuildOption == BuildOption.Remote)
             {
-                if(GlobalCoreToolsSettings.CurrentWorkerRuntime == WorkerRuntime.Python && functionApp.FunctionAppConfig?.Runtime?.Version.Equals($"3.14"))
+                if (GlobalCoreToolsSettings.CurrentWorkerRuntime == WorkerRuntime.Python && functionApp.FunctionAppConfig?.Runtime?.Version == "3.14")
                 {
                     // Remote build for Python 3.14 is not supported on Flex consumption
                     throw new CliException("Remote build for Python 3.14 is not yet supported for Flex. Please refer to <AKA.MS link> for more details.");
                 }
+
                 deploymentParameters.Add("RemoteBuild", true.ToString());
             }
 
