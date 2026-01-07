b0647e57bc522af49e65409eb18e14f501c3f608
Lilian Kasem <likasem@microsoft.com>
2026-01-07 11:58:24 -0800

    Refactor get bundles path

---
+++ src/Cli/func/Actions/LocalActions/GetBundlePathAction.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Azure.Functions.Cli.Common;
+using Azure.Functions.Cli.ExtensionBundle;
+using Colors.Net;
+using Fclp;
+
+namespace Azure.Functions.Cli.Actions.LocalActions
+{
+    [Action(Name = "path", Context = Context.Bundles, HelpText = "Get the path to the downloaded extension bundle.")]
+    internal class GetBundlePathAction : BaseAction
+    {
+        public string Language { get; set; }
+
+        public override ICommandLineParserResult ParseArgs(string[] args)
+        {
+            return base.ParseArgs(args);
+        }
+
+        public override async Task RunAsync()
+        {
+            var extensionBundleManager = ExtensionBundleHelper.GetExtensionBundleManager();
+            if (extensionBundleManager.IsExtensionBundleConfigured())
+            {
+                try
+                {
+                    var options = ExtensionBundleHelper.GetExtensionBundleOptions();
+                    var bundleBasePath = ExtensionBundleHelper.GetBundleDownloadPath(options.Id);
+                    var bundleDetails = await extensionBundleManager.GetExtensionBundleDetails();
+                    var bundlePath = Path.Combine(bundleBasePath, bundleDetails.Version);
+
+                    if (string.IsNullOrEmpty(bundlePath))
+                    {
+                        throw new CliException("Unable to locate extension bundle.");
+                    }
+                    else
+                    {
+                        ColoredConsole.WriteLine(bundlePath);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new CliException("Unable to locate extension bundle.", e);
+                }
+            }
+            else
+            {
+                ColoredConsole.WriteLine("Extension bundle not configured.");
+            }
+        }
+    }
+}
