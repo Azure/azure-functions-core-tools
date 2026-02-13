f17bf3e49a1a680717a992368c0560e0cf33ab8f
Lilian Kasem <likasem@microsoft.com>
2026-02-13 13:57:16 -0800

    Fix issue

--- src/Cli/func/Actions/HostActions/StartHostAction.cs
+++ src/Cli/func/Actions/HostActions/StartHostAction.cs
@@ -905,8 +905,11 @@ namespace Azure.Functions.Cli.Actions.HostActions
             // Only set environment variables if they're not already defined in env var or host.json (user's config takes precedence)
             // Set the log level for Function.* categories to enable user logs at the specified level
             string functionLogLevelHostJsonPath = "logging:logLevel:Function";
+            bool functionLogLevelDefined = Utilities.JobHostConfigSectionExists(hostJsonConfig, functionLogLevelHostJsonPath)
+                || hostJsonConfig.GetSection(functionLogLevelHostJsonPath).Exists();
+
             if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(Constants.FunctionLogLevel)) &&
-                !Utilities.JobHostConfigSectionExists(hostJsonConfig, functionLogLevelHostJsonPath))
+                !functionLogLevelDefined)
             {
                 Environment.SetEnvironmentVariable(Constants.FunctionLogLevel, loggingFilterHelper.UserLogDefaultLogLevel.ToString());
             }
