bc0681d6917aa1a4ff4217a408f70342fac160e2
v-umangsr <v-umangsr@DESKTOP-VK97IEP>
2026-04-28 01:11:35 +0530

    #4751 - Fix: resolve storage account lookup failure in func fetch-connection-string

--- test/Cli/Func.UnitTests/HelperTests/AzureHelperTests.cs
+++ test/Cli/Func.UnitTests/HelperTests/AzureHelperTests.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Azure.Functions.Cli.Arm;
+using Azure.Functions.Cli.Arm.Models;
 using Azure.Functions.Cli.Helpers;
 using FluentAssertions;
+using Moq;
 using RichardSzalay.MockHttp;
 using Xunit;
 
@@ -33,6 +35,102 @@ namespace Azure.Functions.Cli.UnitTests.HelperTests
 
                 var result = await AzureHelper.GetFunctionKey(functionName, AppId, AccessToken, ManagementUrl);
                 result.Should().Be(value);
+            }
+            finally
+            {
+                ArmClient.SetTestHandler(null);
+            }
+        }
+
+        [Fact]
+        public async Task GetStorageAccount_ShouldReturn_WhenStorageExists()
+        {
+            try
+            {
+                const string managementUrl = "https://management.azure.com";
+                const string subscriptionId = "sub";
+                const string storageName = "mystorage";
+                const string resourceGroup = "rg";
+                const string accessToken = "token";
+
+                var listUrl =
+                    $"{managementUrl}/subscriptions/{subscriptionId}/providers/Microsoft.Storage/storageAccounts?api-version=2018-02-01";
+
+                var getUrl =
+                    $"{managementUrl}/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.Storage/storageAccounts/{storageName}?api-version=2018-02-01";
+
+                var keysUrl =
+                    $"{managementUrl}/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.Storage/storageAccounts/{storageName}/listKeys?api-version=2018-02-01";
+
+                var subscriptionsUrl =
+                    $"{managementUrl}/subscriptions?api-version=2018-09-01";
+
+                var mockHttp = new MockHttpMessageHandler();
+
+                // Mock subscriptions
+                mockHttp.When(HttpMethod.Get, subscriptionsUrl).Respond("application/json", @"{'value': [{ 'subscriptionId': 'sub', 'displayName': 'Test Sub' }]}");
+
+                // Mock list storage accounts
+                mockHttp.When(HttpMethod.Get, listUrl).Respond("application/json", @"{'value': [{'name': 'mystorage','id': '/subscriptions/sub/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/mystorage'}]}");
+
+                // Mock get storage account
+                mockHttp.When(HttpMethod.Get, getUrl).Respond("application/json", @"{'name': 'mystorage','id': '/subscriptions/sub/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/mystorage'}");
+
+                // Mock listKeys
+                mockHttp.When(HttpMethod.Post, keysUrl).Respond("application/json", @"{'keys': [{'keyName': 'key1','value': 'test-key','permissions': 'FULL'}]}");
+
+                // Attach mock handler
+                ArmClient.SetTestHandler(mockHttp);
+
+                // Act
+                var result = await AzureHelper.GetStorageAccount(
+                    storageName,
+                    accessToken,
+                    managementUrl);
+
+                // Assert
+                result.Should().NotBeNull();
+                result.StorageAccountName.Should().Be(storageName);
+                result.StorageAccountKey.Should().Be("test-key");
+            }
+            finally
+            {
+                ArmClient.SetTestHandler(null);
+            }
+        }
+
+        [Fact]
+        public async Task GetStorageAccount_ShouldThrow_WhenStorageNotFound()
+        {
+            try
+            {
+                const string managementUrl = "https://management.azure.com";
+                const string subscriptionId = "sub";
+                const string storageName = "mystorage";
+                const string accessToken = "token";
+
+                var subscriptionsUrl =
+                    $"{managementUrl}/subscriptions?api-version=2018-09-01";
+
+                var listUrl =
+                    $"{managementUrl}/subscriptions/{subscriptionId}/providers/Microsoft.Storage/storageAccounts?api-version=2018-02-01";
+
+                var mockHttp = new MockHttpMessageHandler();
+
+                // Mock subscriptions
+                mockHttp.When(HttpMethod.Get, subscriptionsUrl).Respond("application/json", @"{'value': [{ 'subscriptionId': 'sub', 'displayName': 'Test Sub' }]}");
+
+                // Mock empty storage accounts list
+                mockHttp.When(HttpMethod.Get, listUrl).Respond("application/json", @"{ 'value': [] }");
+
+                ArmClient.SetTestHandler(mockHttp);
+
+                // Act
+                Func<Task> act = async () =>
+                    await AzureHelper.GetStorageAccount(storageName, accessToken, managementUrl);
+
+                // Assert
+                await Assert.ThrowsAsync<ArmResourceNotFoundException>(() => AzureHelper.GetStorageAccount(storageName, accessToken, managementUrl));
             }
             finally
             {
