fe949d8f844ec76a052227ca7a0193cf705ec38a
Lilian Kasem <likasem@microsoft.com>
2026-05-01 14:16:08 -0700

    Add workload entry-point discovery

--- test/Workload.Tests.Fixtures.Default/StubWorkload.cs
+++ test/Workload.Tests.Fixtures.Default/StubWorkload.cs
@@ -1,7 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Azure.Functions.Cli.Workload.Tests.Fixtures.Default;
 using Azure.Functions.Cli.Workloads;
+
+[assembly: ExportCliWorkload<StubWorkload>]
 
 namespace Azure.Functions.Cli.Workload.Tests.Fixtures.Default;
 
