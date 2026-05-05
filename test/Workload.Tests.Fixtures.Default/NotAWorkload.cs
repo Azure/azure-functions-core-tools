5a6923181f9b8ea2e64f44621453b9ed7b1d8f1b
Lilian Kasem <likasem@microsoft.com>
2026-05-05 11:53:36 -0700

    Rename workload test fixtures to drop Func. prefix

--- test/Workload.Tests.Fixtures.Default/NotAWorkload.cs
+++ test/Workload.Tests.Fixtures.Default/NotAWorkload.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-namespace Azure.Functions.Cli.Tests.Fixtures.Workload;
+namespace Azure.Functions.Cli.Workload.Tests.Fixtures.Default;
 
 public sealed class NotAWorkload
 {
