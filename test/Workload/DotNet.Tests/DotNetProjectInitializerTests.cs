cc17a9412af995a3195aea57b916d813f757a3c8
Sarah Vu <sarahvu@microsoft.com>
2026-05-15 12:05:42 -0700

    add back refs to fix tests

--- test/Workload/DotNet.Tests/DotNetProjectInitializerTests.cs
+++ test/Workload/DotNet.Tests/DotNetProjectInitializerTests.cs
@@ -1,11 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.CommandLine;
 using Azure.Functions.Cli.Common;
-using Azure.Functions.Cli.Workloads.DotNet;
 using Azure.Functions.Cli.Workloads;
 using Microsoft.Extensions.DependencyInjection;
-using System.CommandLine;
 using Xunit;
 
 namespace Azure.Functions.Cli.Workload.DotNet.Tests;
