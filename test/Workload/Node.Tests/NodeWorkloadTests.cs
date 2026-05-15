8b175d5507a1628a57fde45dc1c1450f64cf59b8
Lilian Kasem <likasem@microsoft.com>
2026-05-15 12:27:11 -0700

    Scaffold Node workload (#4999)

---
+++ test/Workload/Node.Tests/NodeWorkloadTests.cs
@@ -0,0 +1,34 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Azure.Functions.Cli.Workloads;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Xunit;
+
+namespace Azure.Functions.Cli.Workload.Node.Tests;
+
+public class NodeWorkloadTests
+{
+    [Fact]
+    public void Configure_RegistersProjectInitializer()
+    {
+        ServiceCollection services = new();
+        FunctionsCliBuilder builder = Substitute.For<FunctionsCliBuilder>();
+        builder.Services.Returns(services);
+
+        new NodeWorkload().Configure(builder);
+
+        ServiceProvider provider = services.BuildServiceProvider();
+        IProjectInitializer initializer = provider.GetRequiredService<IProjectInitializer>();
+        Assert.IsType<NodeProjectInitializer>(initializer);
+        Assert.Equal("node", initializer.Stack);
+        Assert.Equal(["JavaScript", "TypeScript"], initializer.SupportedLanguages);
+    }
+
+    [Fact]
+    public void Configure_NullBuilder_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new NodeWorkload().Configure(null!));
+    }
+}
