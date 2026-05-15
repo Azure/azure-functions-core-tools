3243a0a351e587bde4f0846b1ad57eb1135d2dc5
Sarah Vu <sarahvu@microsoft.com>
2026-05-15 12:17:32 -0700

    test fix

--- test/Workload/DotNet.Tests/DotNetProjectInitializerTests.cs
+++ test/Workload/DotNet.Tests/DotNetProjectInitializerTests.cs
@@ -4,7 +4,6 @@
 using System.CommandLine;
 using Azure.Functions.Cli.Common;
 using Azure.Functions.Cli.Workloads;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Azure.Functions.Cli.Workload.DotNet.Tests;
@@ -12,70 +11,28 @@ namespace Azure.Functions.Cli.Workload.DotNet.Tests;
 public class DotNetProjectInitializerTests
 {
     [Fact]
-    public void Configure_RegistersProjectInitializer()
+    public async Task InitializeAsync_Throws_NotImplemented()
     {
-        // Arrange
-        var services = new ServiceCollection();
-        var builder = new TestBuilder(services);
-        var workload = new DotNetWorkload();
-
-        // Act
-        workload.Configure(builder);
-
-        // Assert
-        var serviceDescriptor = services.FirstOrDefault(sd => sd.ServiceType == typeof(IProjectInitializer));
-        Assert.NotNull(serviceDescriptor);
-        Assert.Equal(typeof(DotNetProjectInitializer), serviceDescriptor.ImplementationType);
-    }
-
-    [Fact]
-    public void Stack_ReturnsDotnet()
-    {
-        // Arrange
         var initializer = new DotNetProjectInitializer();
+        var context = new InitContext(
+            WorkingDirectory.FromExplicit(Path.GetTempPath()),
+            ProjectName: "test",
+            Language: null,
+            Force: false);
 
-        // Act & Assert
-        Assert.Equal("dotnet", initializer.Stack);
-    }
-
-    [Fact]
-    public void SupportedLanguages_ReturnsCSharpAndFSharp()
-    {
-        // Arrange
-        var initializer = new DotNetProjectInitializer();
-
-        // Act
-        var languages = initializer.SupportedLanguages;
-
-        // Assert
-        Assert.NotNull(languages);
-        Assert.Equal(2, languages.Count);
-        Assert.Contains("C#", languages);
-        Assert.Contains("F#", languages);
-    }
-
-    [Fact]
-    public async Task InitializeAsync_ThrowsNotImplementedException()
-    {
-        // Arrange
-        var initializer = new DotNetProjectInitializer();
-        var workingDir = WorkingDirectory.FromCwd();
-        var context = new InitContext(workingDir, "TestProject", "C#", Force: false);
-        var command = new RootCommand();
-        var parseResult = command.Parse("");
-
-        // Act & Assert
         await Assert.ThrowsAsync<NotImplementedException>(
-            () => initializer.InitializeAsync(context, parseResult));
+            () => initializer.InitializeAsync(context, new RootCommand().Parse(string.Empty)));
     }
 
-    private sealed class TestBuilder(IServiceCollection services) : FunctionsCliBuilder
+    [Fact]
+    public void Stack_IsDotNet()
     {
-        public override IServiceCollection Services { get; } = services;
+        Assert.Equal("dotnet", new DotNetProjectInitializer().Stack);
+    }
 
-        protected override void OnRegisterCommand(Func<IServiceProvider, FuncCommand> factory)
-        {
-            // Test implementation - no-op for basic service registration tests
-        }
+    [Fact]
+    public void GetInitOptions_IsEmpty()
+    {
+        Assert.Empty(new DotNetProjectInitializer().GetInitOptions());
     }
 }
