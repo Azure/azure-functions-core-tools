87916544854e28d7c44adc7bb38dac90612a8c77
Sarah Vu <sarahvu@microsoft.com>
2026-05-18 16:31:13 -0700

    handle hive cache, add --force, move constant

--- src/Func/Common/Constants.cs
+++ src/Func/Common/Constants.cs
@@ -28,11 +28,6 @@ internal static class Constants
     public const string VersionCacheFileName = ".version-check";
 
     /// <summary>
-    /// Directory name (under the user profile) the func CLI persists state in.
-    /// </summary>
-    public const string FuncHomeDirectoryName = ".azure-functions";
-
-    /// <summary>
     /// Prefix for environment variables that bind into <c>IConfiguration</c>.
     /// e.g. <c>FUNC_CLI_Workloads__Home</c> binds <c>Workloads:Home</c>.
     /// </summary>
