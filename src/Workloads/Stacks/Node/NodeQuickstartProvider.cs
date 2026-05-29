fee2a950d8d2a6921f380bbd22154b20d70f14a7
manvkaur <67894494+manvkaur@users.noreply.github.com>
2026-05-29 15:58:10 -0700

    revert host json hint to generic

--- src/Workloads/Stacks/Node/NodeQuickstartProvider.cs
+++ src/Workloads/Stacks/Node/NodeQuickstartProvider.cs
@@ -28,6 +28,6 @@ internal sealed class NodeQuickstartProvider : IQuickstartProvider
     public IReadOnlyList<string> GetNextSteps(string manifestLanguage) =>
     [
         "Run `npm install` to install dependencies",
-        "Run `func run [path]` to launch the app"
+        "Run `func run [path]` where path is folder containing host.json to launch the app"
     ];
 }
